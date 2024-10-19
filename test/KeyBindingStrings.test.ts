3bb6c83e6800951a7e249ddb19cc9aed1904f102
Le Vivilet <levivlet@pm.me>
2024-10-19 14:54:56 +0200

    test

--- test/KeyBindingStrings.test.ts
+++ test/KeyBindingStrings.test.ts
@@ -20,3 +20,47 @@ test('key', () => {
 test('typeToSearchKeyBinding', () => {
   expect(KeyBindingStrings.typeToSearchKeyBindings()).toBe('Type to search in keybindings')
 })
+
+test('pressDesiredKeyCombinationThenPressEnter', () => {
+  expect(KeyBindingStrings.pressDesiredKeyCombinationThenPressEnter()).toBe('Press Desired Key Combination, Then Press Enter')
+})
+
+test('resultsWillUpdateAsYouType', () => {
+  expect(KeyBindingStrings.resultsWillUpdateAsYouType()).toBe('Results will update as you type')
+})
+
+test('copy', () => {
+  expect(KeyBindingStrings.copy()).toBe('Copy')
+})
+
+test('copyCommandId', () => {
+  expect(KeyBindingStrings.copyCommandId()).toBe('Copy Command ID')
+})
+
+test('copyCommandTitle', () => {
+  expect(KeyBindingStrings.copyCommandTitle()).toBe('Copy Command Title')
+})
+
+test('changeKeyBinding', () => {
+  expect(KeyBindingStrings.changeKeyBinding()).toBe('Change KeyBinding')
+})
+
+test('addKeyBinding', () => {
+  expect(KeyBindingStrings.addKeyBinding()).toBe('Add KeyBinding')
+})
+
+test('removeKeyBinding', () => {
+  expect(KeyBindingStrings.removeKeyBinding()).toBe('Remove KeyBinding')
+})
+
+test('resetKeyBinding', () => {
+  expect(KeyBindingStrings.resetKeyBinding()).toBe('Reset KeyBinding')
+})
+
+test('changeWhenExpression', () => {
+  expect(KeyBindingStrings.changeWhenExpression()).toBe('Change When Expression')
+})
+
+test('showSameKeyBindings', () => {
+  expect(KeyBindingStrings.showSameKeyBindings()).toBe('Show Same KeyBindings')
+})
