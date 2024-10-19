b1e79367360a13b3da94e4b28ef2f13bcc64933a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-19 17:17:44 +0200

    feature: add render commands (#8)
    * feature: add render commands
    
    * coverage
    
    * test

---
+++ src/parts/GetVisibleKeyBindings/GetVisibleKeyBindings.ts
@@ -0,0 +1,23 @@
+import * as GetWhenExpressionText from '../GetWhenExpressionText/GetWhenExpressionText.ts'
+
+export const getVisibleKeyBindings = (filteredKeyBindings: any, minLineY: number, maxLineY: number, selectedIndex: number) => {
+  const visibleKeyBindings = []
+  const slicedKeyBindings = filteredKeyBindings.slice(minLineY, maxLineY)
+  const relativeSelectedIndex = selectedIndex - minLineY
+  for (let i = 0; i < slicedKeyBindings.length; i++) {
+    const slicedKeyBinding = slicedKeyBindings[i]
+    const { isCtrl, isShift, key, when, command, commandMatches, keyMatches } = slicedKeyBinding
+    visibleKeyBindings.push({
+      rowIndex: minLineY + i + 2,
+      isCtrl,
+      isShift,
+      key,
+      when: GetWhenExpressionText.getWhenExpressionText(when),
+      command,
+      selected: i === relativeSelectedIndex,
+      commandMatches,
+      keyMatches,
+    })
+  }
+  return visibleKeyBindings
+}
