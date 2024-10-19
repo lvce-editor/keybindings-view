a56e9511db2e02661c383f8a6d5a48e5779dab28
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-19 15:22:30 +0200

    feature: add types for parsed key (#6)

--- src/parts/ParseKey/ParseKey.ts
+++ src/parts/ParseKey/ParseKey.ts
@@ -1,8 +1,9 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as GetKeyCodeString from '../GetKeyCodeString/GetKeyCodeString.ts'
 import * as KeyModifier from '../KeyModifier/KeyModifier.ts'
+import type { ParsedKey } from '../ParsedKey/ParsedKey.ts'
 
-export const parseKey = (rawKey: any) => {
+export const parseKey = (rawKey: number): ParsedKey => {
   Assert.number(rawKey)
   const isCtrl = Boolean(rawKey & KeyModifier.CtrlCmd)
   const isShift = Boolean(rawKey & KeyModifier.Shift)
