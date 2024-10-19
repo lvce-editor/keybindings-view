236cd6646fee0ede43d7bab816253f697e323f06
Le Vivilet <levivlet@pm.me>
2024-10-19 15:08:58 +0200

    remove unused code

--- src/parts/Px/Px.ts
+++ src/parts/Px/Px.ts
@@ -2,6 +2,6 @@ export const px = (value: number): string => {
   return `${value}px`
 }
 
-export const position = (x: number, y: number): string => {
+const position = (x: number, y: number): string => {
   return `${x}px ${y}px`
 }
--- src/parts/Rpc/Rpc.ts
+++ src/parts/Rpc/Rpc.ts
@@ -1,7 +1,7 @@
 import * as IpcState from '../IpcState/IpcState.ts'
 import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
 
-export const invoke = (method: string, ...params: any[]) => {
+const invoke = (method: string, ...params: any[]) => {
   const ipc = IpcState.get()
   return JsonRpc.invoke(ipc, method, ...params)
 }
