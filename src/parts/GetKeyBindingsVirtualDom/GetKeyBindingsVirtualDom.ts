8bf5177dfb3baec680506aa73a70f7af026f724f
Le Vivilet <levivlet@pm.me>
2024-10-31 09:29:15 +0100

    feature: move keybindings scrollbar next to table

--- src/parts/GetKeyBindingsVirtualDom/GetKeyBindingsVirtualDom.ts
+++ src/parts/GetKeyBindingsVirtualDom/GetKeyBindingsVirtualDom.ts
@@ -1,10 +1,9 @@
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetKeyBindingsHeaderVirtualDom from '../GetKeyBindingsHeaderVirtualDom/GetKeyBindingsHeaderVirtualDom.ts'
-import * as GetKeyBindingsTableVirtualDom from '../GetKeyBindingsTableVirtualDom/GetKeyBindingsTableVirtualDom.ts'
-import * as GetScrollBarVirtualDom from '../GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import * as GetKeyBindingsTableWrapperVirtualDom from '../GetKeyBindingsTableWrapperVirtualDom/GetKeyBindingsTableWrapperVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getKeyBindingsVirtualDom = (
   filteredKeyBindings: any[],
@@ -21,26 +20,17 @@ export const getKeyBindingsVirtualDom = (
       className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.KeyBindings),
       onPointerDown: DomEventListenerFunctions.HandlePointerDown,
       onDblClick: DomEventListenerFunctions.HandleTableDoubleClick,
-      onWheel: DomEventListenerFunctions.HandleWheel,
-      childCount: 3,
+      childCount: 2,
     },
     ...GetKeyBindingsHeaderVirtualDom.getKeyBindingsHeaderVirtualDom(),
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.KeyBindingsTableWrapper,
-      childCount: 3,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.Resizer,
-      childCount: 0,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.Resizer,
-      childCount: 0,
-    },
-    ...GetKeyBindingsTableVirtualDom.getTableDom(filteredKeyBindings, displayKeyBindings, columnWidth1, columnWidth2, columnWidth3),
-    ...GetScrollBarVirtualDom.getScrollBarVirtualDom(scrollBarThumbHeight, scrollBarThumbTop),
+    ...GetKeyBindingsTableWrapperVirtualDom.getKeyBindingsTableWrapperVirtualDom(
+      filteredKeyBindings,
+      displayKeyBindings,
+      columnWidth1,
+      columnWidth2,
+      columnWidth3,
+      scrollBarThumbHeight,
+      scrollBarThumbTop
+    ),
   ]
 }
