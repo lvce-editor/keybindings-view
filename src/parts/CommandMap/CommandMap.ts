a25681398bbf5a30ff6361af231de6f5aee40eaa
Le Vivilet <levivlet@pm.me>
2024-10-31 09:46:17 +0100

    feature: add handleResizerMove command

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,6 @@
 import * as FilterKeyBindings from '../FilterKeyBindings/FilterKeyBindings.ts'
 import * as GetKeyBindingsVirtualDom from '../GetKeyBindingsVirtualDom/GetKeyBindingsVirtualDom.ts'
+import * as HandleResizerMove from '../HandleResizerMove/HandleResizerMove.ts'
 import * as ParseKeyBindings from '../ParseKeyBindings/ParseKeyBindings.ts'
 import * as Render from '../Render/Render.ts'
 
@@ -10,8 +11,9 @@ export const commandMap = {
   'ParseKeyBindings.parseKeyBindings': ParseKeyBindings.parseKeyBindings,
 
   // new
-  'KeyBindings.render': Render.getRenderCommands,
-  'KeyBindings.parse': ParseKeyBindings.parseKeyBindings,
   'KeyBindings.filter': FilterKeyBindings.getFilteredKeyBindings,
   'KeyBindings.getDom': GetKeyBindingsVirtualDom.getKeyBindingsVirtualDom,
+  'KeyBindings.handleResizerMove': HandleResizerMove.handleResizerMove,
+  'KeyBindings.parse': ParseKeyBindings.parseKeyBindings,
+  'KeyBindings.render': Render.getRenderCommands,
 }
