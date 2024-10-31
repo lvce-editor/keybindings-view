01f76f8d221a4cbc5306ebac74ab010c48c78c02
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-31 09:30:13 +0100

    feature: move keybindings scrollbar next to table (#11)

---
+++ src/parts/GetKeyBindingsTableWrapperVirtualDom/GetKeyBindingsTableWrapperVirtualDom.ts
@@ -0,0 +1,36 @@
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as GetKeyBindingsTableVirtualDom from '../GetKeyBindingsTableVirtualDom/GetKeyBindingsTableVirtualDom.ts'
+import * as GetScrollBarVirtualDom from '../GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getKeyBindingsTableWrapperVirtualDom = (
+  filteredKeyBindings: any[],
+  displayKeyBindings: any,
+  columnWidth1: number,
+  columnWidth2: number,
+  columnWidth3: number,
+  scrollBarThumbHeight: number,
+  scrollBarThumbTop: number
+) => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.KeyBindingsTableWrapper,
+      onWheel: DomEventListenerFunctions.HandleWheel,
+      childCount: 4,
+    },
+    ...GetKeyBindingsTableVirtualDom.getTableDom(filteredKeyBindings, displayKeyBindings, columnWidth1, columnWidth2, columnWidth3),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Resizer,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Resizer,
+      childCount: 0,
+    },
+    ...GetScrollBarVirtualDom.getScrollBarVirtualDom(scrollBarThumbHeight, scrollBarThumbTop),
+  ]
+}
