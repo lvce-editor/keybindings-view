0833ce2f4b1da375f9573e82715c10eb392be39a
Le Vivilet <levivlet@pm.me>
2024-11-16 21:34:47 +0100

    feature: use new rpc

--- src/parts/Listen/Listen.ts
+++ src/parts/Listen/Listen.ts
@@ -1,13 +1,8 @@
-import * as Command from '../Command/Command.ts'
+import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
-import * as IpcChild from '../IpcChild/IpcChild.ts'
-import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
-import * as Rpc from '../Rpc/Rpc.ts'
 
-export const listen = async () => {
-  Command.register(CommandMap.commandMap)
-  const ipc = await IpcChild.listen({ method: IpcChildType.Auto() })
-  HandleIpc.handleIpc(ipc)
-  Rpc.listen(ipc)
+export const listen = async (): Promise<void> => {
+  await WebWorkerRpcClient.create({
+    commandMap: CommandMap.commandMap,
+  })
 }
