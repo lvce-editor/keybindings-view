9b11bd17bca96b4b7e24f54e9d1e436b10940a2f
Le Vivilet <levivlet@pm.me>
2024-11-25 18:45:09 +0100

    feature: add getKeyBindings function

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,7 @@
 import * as FilterKeyBindings from '../FilterKeyBindings/FilterKeyBindings.ts'
 import * as GetKeyBindingsVirtualDom from '../GetKeyBindingsVirtualDom/GetKeyBindingsVirtualDom.ts'
 import * as HandleResizerMove from '../HandleResizerMove/HandleResizerMove.ts'
+import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as ParseKeyBindings from '../ParseKeyBindings/ParseKeyBindings.ts'
 import * as Render from '../Render/Render.ts'
 
@@ -13,6 +14,7 @@ export const commandMap = {
   // new
   'KeyBindings.filter': FilterKeyBindings.getFilteredKeyBindings,
   'KeyBindings.getDom': GetKeyBindingsVirtualDom.getKeyBindingsVirtualDom,
+  'KeyBindings.getKeyBindings': GetKeyBindings.getKeyBindings,
   'KeyBindings.handleResizerMove': HandleResizerMove.handleResizerMove,
   'KeyBindings.parse': ParseKeyBindings.parseKeyBindings,
   'KeyBindings.render': Render.getRenderCommands,
