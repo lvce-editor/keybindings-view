19f4a682bf2bcb096fc9ae042ec7867075a6beab
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-18 23:08:48 +0100

    feature: add focusFirst command (#30)

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,8 @@
 import * as FilterKeyBindings from '../FilterKeyBindings/FilterKeyBindings.ts'
+import * as FocusFirst from '../FocusFirst/FocusFirst.ts'
+import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as GetKeyBindingsVirtualDom from '../GetKeyBindingsVirtualDom/GetKeyBindingsVirtualDom.ts'
 import * as HandleResizerMove from '../HandleResizerMove/HandleResizerMove.ts'
-import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as ParseKeyBindings from '../ParseKeyBindings/ParseKeyBindings.ts'
 import * as Render from '../Render/Render.ts'
 import * as FocusLast from '../FocusLast/FocusLast.ts'
@@ -19,5 +20,6 @@ export const commandMap = {
   'KeyBindings.handleResizerMove': HandleResizerMove.handleResizerMove,
   'KeyBindings.parse': ParseKeyBindings.parseKeyBindings,
   'KeyBindings.render': Render.getRenderCommands,
+  'KeyBindings.focusFirst': FocusFirst.focusFirst,
   'KeyBindings.focusLast': FocusLast.focusLast,
 }
