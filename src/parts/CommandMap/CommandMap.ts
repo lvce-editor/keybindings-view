2805c60e6553b61a9d129e09afb6193a50ad9267
Le Vivilet <levivlet@pm.me>
2024-12-18 23:13:43 +0100

    feature: add click command

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,12 +1,13 @@
 import * as FilterKeyBindings from '../FilterKeyBindings/FilterKeyBindings.ts'
-import * as FocusPrevious from '../FocusPrevious/FocusPrevious.ts'
 import * as FocusFirst from '../FocusFirst/FocusFirst.ts'
+import * as FocusLast from '../FocusLast/FocusLast.ts'
+import * as FocusPrevious from '../FocusPrevious/FocusPrevious.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as GetKeyBindingsVirtualDom from '../GetKeyBindingsVirtualDom/GetKeyBindingsVirtualDom.ts'
+import * as HandleClick from '../HandleClick/HandleClick.ts'
 import * as HandleResizerMove from '../HandleResizerMove/HandleResizerMove.ts'
 import * as ParseKeyBindings from '../ParseKeyBindings/ParseKeyBindings.ts'
 import * as Render from '../Render/Render.ts'
-import * as FocusLast from '../FocusLast/FocusLast.ts'
 
 export const commandMap = {
   // deprecated
@@ -16,12 +17,13 @@ export const commandMap = {
 
   // new
   'KeyBindings.filter': FilterKeyBindings.getFilteredKeyBindings,
+  'KeyBindings.focusFirst': FocusFirst.focusFirst,
+  'KeyBindings.focusLast': FocusLast.focusLast,
+  'KeyBindings.focusPrevious': FocusPrevious.focusPrevious,
   'KeyBindings.getDom': GetKeyBindingsVirtualDom.getKeyBindingsVirtualDom,
   'KeyBindings.getKeyBindings': GetKeyBindings.getKeyBindings,
+  'KeyBindings.handleClick': HandleClick.handleClick,
   'KeyBindings.handleResizerMove': HandleResizerMove.handleResizerMove,
   'KeyBindings.parse': ParseKeyBindings.parseKeyBindings,
   'KeyBindings.render': Render.getRenderCommands,
-  'KeyBindings.focusPrevious': FocusPrevious.focusPrevious,
-  'KeyBindings.focusFirst': FocusFirst.focusFirst,
-  'KeyBindings.focusLast': FocusLast.focusLast,
 }
