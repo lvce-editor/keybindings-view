918900774b790a20f16b4290401c2d816cc81ab5
Le Vivilet <levivlet@pm.me>
2024-12-19 20:16:41 +0100

    lint

--- packages/keybindings-view/src/parts/GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts
@@ -1,7 +1,7 @@
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as Px from '../Px/Px.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getScrollBarVirtualDom = (scrollBarThumbHeight: number, scrollBarThumbTop: number): readonly VirtualDomNode[] => {
   return [
