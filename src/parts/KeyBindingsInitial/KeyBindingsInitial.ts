0ab29da3b86b04f07a6984533922e014805600c1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 19:17:47 +0100

    Delete src/parts/KeyBindingsInitial/KeyBindingsInitial.ts

--- src/parts/KeyBindingsInitial/KeyBindingsInitial.ts
+++
@@ -1,5 +0,0 @@
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
-
-export const getKeyBindings = (): Promise<readonly any[]> => {
-  return ParentRpc.invoke('KeyBindingsInitial.getKeyBindings')
-}
