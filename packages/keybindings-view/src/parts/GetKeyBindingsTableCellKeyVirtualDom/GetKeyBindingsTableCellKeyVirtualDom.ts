2e378eae7367c5ecbf4a63ec0c388cc455ac1a71
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 20:04:47 +0100

    feature: format code (#50)

--- packages/keybindings-view/src/parts/GetKeyBindingsTableCellKeyVirtualDom/GetKeyBindingsTableCellKeyVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetKeyBindingsTableCellKeyVirtualDom/GetKeyBindingsTableCellKeyVirtualDom.ts
@@ -39,7 +39,7 @@ export const getKeyBindingsTableCellKeyDom = (keyBinding: any) => {
       className: ClassNames.KeyBindingsTableCell,
       childCount,
     },
-    ...children
+    ...children,
   )
   return dom
 }
