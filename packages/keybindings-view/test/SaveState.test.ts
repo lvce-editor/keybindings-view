dec1342ca6f6afead0bf9741e8e3132238de8f5c
Le Vivilet <levivlet@pm.me>
2024-12-19 20:19:43 +0100

    feature: add saveState test

---
+++ packages/keybindings-view/test/SaveState.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@jest/globals'
+import * as SaveState from '../src/parts/SaveState/SaveState.ts'
+
+test('saveState', () => {
+  const state: any = {
+    value: 'test',
+    otherProperty: 123,
+    anotherProperty: 'abc',
+  }
+  expect(SaveState.saveState(state)).toEqual({
+    value: 'test',
+  })
+})
