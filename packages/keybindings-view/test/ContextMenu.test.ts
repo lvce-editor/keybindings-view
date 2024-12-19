88fc1569dc6b62b9aa7d2d26e2945fb4673067b5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 20:34:13 +0100

    feature: add contextMenu test (#62)
    * feature: add contextMenu test
    
    * test

---
+++ packages/keybindings-view/test/ContextMenu.test.ts
@@ -0,0 +1,21 @@
+import { beforeEach, expect, jest, test } from '@jest/globals'
+
+const mockParentRpc = {
+  invoke: jest.fn(),
+}
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/ParentRpc/ParentRpc.ts', () => mockParentRpc)
+
+const ContextMenu = await import('../src/parts/ContextMenu/ContextMenu.ts')
+
+test('show - invokes context menu with correct items', async () => {
+  const x = 100
+  const y = 200
+  const id = 1
+  await ContextMenu.show(x, y, id)
+  expect(mockParentRpc.invoke).toHaveBeenCalledWith('ContextMenu.show', x, y, id)
+})
