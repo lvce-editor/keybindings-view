8c8877409e80f7b1d1b06200b2c50032e9240d62
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 19:12:45 +0100

    feature: send contextMenu command when opening context menu (#39)
    * feature: send contextMenu command when opening context menu
    
    * cover

--- src/parts/HandleContextMenu/HandleContextMenu.ts
+++ src/parts/HandleContextMenu/HandleContextMenu.ts
@@ -1,6 +1,8 @@
 import { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
+import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 
 export const handleContextMenu = async (state: KeyBindingsState, button: number, x: number, y: number): Promise<KeyBindingsState> => {
-  // await ContextMenu.show(x, y, MenuEntryId.KeyBindingsTable)
+  await ContextMenu.show(x, y, MenuEntryId.KeyBindingsTable)
   return state
 }
