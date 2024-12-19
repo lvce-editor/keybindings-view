f2c68f944a1fda0580a0868674692e7d9b823bff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 20:27:36 +0100

    feature: add getScrollBarVirtualDom tests (#64)

---
+++ packages/keybindings-view/test/GetScrollBarVirtualDom.test.ts
@@ -0,0 +1,41 @@
+import { expect, test } from '@jest/globals'
+import * as GetScrollBarVirtualDom from '../src/parts/GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+
+test('getScrollBarVirtualDom', () => {
+  const scrollBarThumbHeight = 100
+  const scrollBarThumbTop = 50
+
+  expect(GetScrollBarVirtualDom.getScrollBarVirtualDom(scrollBarThumbHeight, scrollBarThumbTop)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ScrollBar,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ScrollBarThumb,
+      childCount: 0,
+      height: '100px',
+      top: '50px',
+    },
+  ])
+})
+
+test('getScrollBarVirtualDom - zero values', () => {
+  expect(GetScrollBarVirtualDom.getScrollBarVirtualDom(0, 0)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ScrollBar,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ScrollBarThumb,
+      childCount: 0,
+      height: '0px',
+      top: '0px',
+    },
+  ])
+})
