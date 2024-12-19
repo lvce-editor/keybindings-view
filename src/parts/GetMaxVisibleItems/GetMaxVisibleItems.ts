26fbabc9c5c05fd73007fb591f2f784eeed7af4c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 19:17:27 +0100

    Delete src/parts/GetMaxVisibleItems/GetMaxVisibleItems.ts

--- src/parts/GetMaxVisibleItems/GetMaxVisibleItems.ts
+++
@@ -1,3 +0,0 @@
-export const getMaxVisibleItems = (height: number, searchHeaderHeight: number, tableHeaderHeight: number, rowHeight: number): number => {
-  return Math.floor((height - searchHeaderHeight - tableHeaderHeight) / rowHeight)
-}
