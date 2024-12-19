d7291bbfc3ab7e0d171786bdeafbe26f19fa2526
Le Vivilet <levivlet@pm.me>
2024-12-19 20:31:26 +0100

    feature: add keybindingsTableRow test

---
+++ packages/keybindings-view/test/GetKeyBindingsTableBodyRowClassName.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@jest/globals'
+import * as GetKeyBindingsTableBodyRowClassName from '../src/parts/GetKeyBindingsTableBodyRowClassName/GetKeyBindingsTableBodyRowClassName.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+
+test('getRowClassName - even row not selected', () => {
+  expect(GetKeyBindingsTableBodyRowClassName.getRowClassName(true, false)).toBe(
+    `${ClassNames.KeyBindingsTableRow} ${ClassNames.KeyBindingsTableRowEven}`,
+  )
+})
+
+test('getRowClassName - odd row not selected', () => {
+  expect(GetKeyBindingsTableBodyRowClassName.getRowClassName(false, false)).toBe(
+    `${ClassNames.KeyBindingsTableRow} ${ClassNames.KeyBindingsTableRowOdd}`,
+  )
+})
+
+test('getRowClassName - even row selected', () => {
+  expect(GetKeyBindingsTableBodyRowClassName.getRowClassName(true, true)).toBe(
+    `${ClassNames.KeyBindingsTableRow} ${ClassNames.KeyBindingsTableRowEven} ${ClassNames.KeyBindingsTableRowSelected}`,
+  )
+})
+
+test('getRowClassName - odd row selected', () => {
+  expect(GetKeyBindingsTableBodyRowClassName.getRowClassName(false, true)).toBe(
+    `${ClassNames.KeyBindingsTableRow} ${ClassNames.KeyBindingsTableRowOdd} ${ClassNames.KeyBindingsTableRowSelected}`,
+  )
+})
