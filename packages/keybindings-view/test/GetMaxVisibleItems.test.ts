9197012b7ab72d1e77c85d26b171798fbbc8dac4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 20:34:47 +0100

    feature: add getMaxVisibleItems test (#61)
    * feature: add getMaxVisibleItems test
    
    * test

---
+++ packages/keybindings-view/test/GetMaxVisibleItems.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@jest/globals'
+import * as GetMaxVisibleItems from '../src/parts/GetMaxVisibleItems/GetMaxVisibleItems.ts'
+
+test('getMaxVisibleItems - exact division', () => {
+  expect(GetMaxVisibleItems.getMaxVisibleItems(100, 20, 20, 20)).toBe(3)
+})
+
+test('getMaxVisibleItems - with remainder', () => {
+  expect(GetMaxVisibleItems.getMaxVisibleItems(105, 20, 20, 20)).toBe(3)
+})
+
+test('getMaxVisibleItems - zero height', () => {
+  expect(GetMaxVisibleItems.getMaxVisibleItems(0, 20, 20, 20)).toBe(-2)
+})
+
+test('getMaxVisibleItems - height smaller than headers', () => {
+  expect(GetMaxVisibleItems.getMaxVisibleItems(30, 20, 20, 20)).toBe(-1)
+})
+
+test('getMaxVisibleItems - large numbers', () => {
+  expect(GetMaxVisibleItems.getMaxVisibleItems(1000, 50, 50, 25)).toBe(36)
+})
