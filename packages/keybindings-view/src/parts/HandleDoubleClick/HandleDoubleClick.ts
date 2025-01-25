b4546ce8f3eea045e9f05522a272e8bdf4feb348
Le Vivilet <levivlet@pm.me>
2025-01-25 18:32:26 +0100

    feature: improve doubleclick command

--- packages/keybindings-view/src/parts/HandleDoubleClick/HandleDoubleClick.ts
+++ packages/keybindings-view/src/parts/HandleDoubleClick/HandleDoubleClick.ts
@@ -1,8 +1,10 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as GetIndex from '../GetIndex/GetIndex.ts'
+import * as ShowDefineKeyBinding from '../ShowDefineKeyBinding/ShowDefineKeyBinding.ts'
 
 export const handleDoubleClick = async (state: KeyBindingsState, eventX: number, eventY: number): Promise<KeyBindingsState> => {
   const selectedIndex = GetIndex.getIndex(state, eventX, eventY)
+  await ShowDefineKeyBinding.showDefineKeyBinding()
   // TODO wait promise?
   // showDefineWidget(state, selectedIndex)
   return {
