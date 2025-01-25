2ee3f9620321f48ddbbd2b7b8da6e0823b4d474d
Le Vivilet <levivlet@pm.me>
2025-01-25 18:40:25 +0100

    feature: add render2 command that keeps state in worker

--- packages/keybindings-view/src/parts/CommandMap/CommandMap.ts
+++ packages/keybindings-view/src/parts/CommandMap/CommandMap.ts
@@ -13,6 +13,7 @@ import * as HandleInput from '../HandleInput/HandleInput.ts'
 import * as HandleResizerMove from '../HandleResizerMove/HandleResizerMove.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as ParseKeyBindings from '../ParseKeyBindings/ParseKeyBindings.ts'
+import * as Render2 from '../Render2/Render2.ts'
 import * as Render from '../Render/Render.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
 import * as SetDeltaY from '../SetDeltaY/SetDeltaY.ts'
@@ -33,6 +34,7 @@ export const commandMap = {
   'KeyBindings.handleResizerMove': WrapCommand.wrapCommand(HandleResizerMove.handleResizerMove),
   'KeyBindings.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
   'KeyBindings.render': Render.getRenderCommands,
+  'KeyBindings.render2': Render2.render2,
   'KeyBindings.saveState': SaveState.saveState,
   'KeyBindings.setDeltaY': WrapCommand.wrapCommand(SetDeltaY.setDeltaY),
 
