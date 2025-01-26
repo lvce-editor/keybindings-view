da558dcc76f26e7fe763f111ae54062cf84d8925
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-26 14:04:35 +0100

    feature: avoid mutation in addHighlights function (#123)

--- packages/keybindings-view/src/parts/GetKeyBindingsTableCellCommandVirtualDom/GetKeyBindingsTableCellCommandVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetKeyBindingsTableCellCommandVirtualDom/GetKeyBindingsTableCellCommandVirtualDom.ts
@@ -1,17 +1,8 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AddHighlights from '../AddHighlights/AddHighlights.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getKeyBindingsTableCellCommandDom = (keyBinding: any): readonly VirtualDomNode[] => {
   const { commandMatches, command } = keyBinding
   const commandHighlights = commandMatches.slice(1)
-  const dom: VirtualDomNode[] = []
-  const tableCell = {
-    type: VirtualDomElements.Td,
-    className: ClassNames.KeyBindingsTableCell,
-    childCount: 0,
-  }
-  AddHighlights.addHighlights(tableCell, dom, commandHighlights, command)
-  return dom
+  return AddHighlights.addHighlights(commandHighlights, command)
 }
