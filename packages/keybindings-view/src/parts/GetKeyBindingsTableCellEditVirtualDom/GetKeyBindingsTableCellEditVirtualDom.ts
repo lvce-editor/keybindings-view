11db7c7157831f2b7e03a307c29c6a7d13757c6b
Le Vivilet <levivlet@pm.me>
2025-01-26 13:44:14 +0100

    feature: simplify edit cell virtual dom

--- packages/keybindings-view/src/parts/GetKeyBindingsTableCellEditVirtualDom/GetKeyBindingsTableCellEditVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetKeyBindingsTableCellEditVirtualDom/GetKeyBindingsTableCellEditVirtualDom.ts
@@ -2,11 +2,11 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetIconVirtualDom from '../GetIconVirtualDom/GetIconVirtualDom.ts'
 import * as Icon from '../Icon/Icon.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getKeyBindingsTableEditCellDom = (): readonly VirtualDomNode[] => {
-  const dom = []
-  dom.push(
+  return [
     {
       type: VirtualDomElements.Td,
       childCount: 1,
@@ -14,10 +14,9 @@ export const getKeyBindingsTableEditCellDom = (): readonly VirtualDomNode[] => {
     },
     {
       type: VirtualDomElements.Button,
-      className: `${ClassNames.IconButton} ${ClassNames.KeyBindingsEditButton}`,
+      className: MergeClassNames.mergeClassNames(ClassNames.IconButton, ClassNames.KeyBindingsEditButton),
       childCount: 1,
     },
     GetIconVirtualDom.getIconVirtualDom(Icon.Edit),
-  )
-  return dom
+  ]
 }
