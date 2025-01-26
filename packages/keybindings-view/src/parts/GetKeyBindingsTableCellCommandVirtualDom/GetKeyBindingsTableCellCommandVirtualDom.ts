25bf99d2ad1120170eca6ee43465485672c1c855
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-26 13:47:12 +0100

    feature: simplify highlight dom (#117)

--- packages/keybindings-view/src/parts/GetKeyBindingsTableCellCommandVirtualDom/GetKeyBindingsTableCellCommandVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetKeyBindingsTableCellCommandVirtualDom/GetKeyBindingsTableCellCommandVirtualDom.ts
@@ -1,36 +1,7 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as AddHighlights from '../AddHighlights/AddHighlights.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
-
-const highlight = {
-  type: VirtualDomElements.Span,
-  className: ClassNames.SearchHighlight,
-  childCount: 1,
-}
-
-const addHighlights = (tableCell: any, dom: any, highlights: any, label: any): void => {
-  dom.push(tableCell)
-  let position = 0
-  for (let i = 0; i < highlights.length; i += 2) {
-    const highlightStart = highlights[i]
-    const highlightEnd = highlights[i + 1]
-    if (position < highlightStart) {
-      const beforeText = label.slice(position, highlightStart)
-      tableCell.childCount++
-      dom.push(text(beforeText))
-    }
-    const highlightText = label.slice(highlightStart, highlightEnd)
-    tableCell.childCount++
-    dom.push(highlight, text(highlightText))
-    position = highlightEnd
-  }
-  if (position < label.length) {
-    const afterText = label.slice(position)
-    tableCell.childCount++
-    dom.push(text(afterText))
-  }
-}
 
 export const getKeyBindingsTableCellCommandDom = (keyBinding: any): readonly VirtualDomNode[] => {
   const { commandMatches, command } = keyBinding
@@ -41,7 +12,6 @@ export const getKeyBindingsTableCellCommandDom = (keyBinding: any): readonly Vir
     className: ClassNames.KeyBindingsTableCell,
     childCount: 0,
   }
-  addHighlights(tableCell, dom, commandHighlights, command)
-  dom.push()
+  AddHighlights.addHighlights(tableCell, dom, commandHighlights, command)
   return dom
 }
