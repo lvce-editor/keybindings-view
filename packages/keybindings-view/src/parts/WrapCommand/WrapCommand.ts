dac98a3c0b091ac13fd203e78d89f23c1e07a696
Le Vivilet <levivlet@pm.me>
2025-01-26 12:46:06 +0100

    feature: remove deprecated wrapCommand part

--- packages/keybindings-view/src/parts/WrapCommand/WrapCommand.ts
+++ packages/keybindings-view/src/parts/WrapCommand/WrapCommand.ts
@@ -2,15 +2,10 @@ import * as KeyBindingsStates from '../KeyBindingsStates/KeyBindingsStates.ts'
 
 export const wrapCommand = (fn: any): any => {
   const wrapped = async (uid: number, ...args: readonly any[]): Promise<void> => {
-    if (typeof uid === 'number') {
       const { newState } = KeyBindingsStates.get(uid)
       const newerState = await fn(newState, ...args)
       KeyBindingsStates.set(uid, newState, newerState)
-    } else {
-      // deprecated
-      const newerState = await fn(uid, ...args)
-      return newerState
-    }
+
   }
   return wrapped
 }
