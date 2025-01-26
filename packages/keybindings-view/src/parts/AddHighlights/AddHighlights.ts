5c036a807ef48e9374b19a1d02234183ed64644f
Le Vivilet <levivlet@pm.me>
2025-01-26 13:56:45 +0100

    feature: add more types

--- packages/keybindings-view/src/parts/AddHighlights/AddHighlights.ts
+++ packages/keybindings-view/src/parts/AddHighlights/AddHighlights.ts
@@ -1,15 +1,16 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
-const highlight = {
+const highlight: VirtualDomNode = {
   type: VirtualDomElements.Span,
   className: ClassNames.SearchHighlight,
   childCount: 1,
 }
 
 // TODO avoid mutation
-export const addHighlights = (tableCell: any, dom: any, highlights: any, label: any): void => {
+export const addHighlights = (tableCell: any, dom: VirtualDomNode[], highlights: readonly number[], label: string): void => {
   dom.push(tableCell)
   let position = 0
   for (let i = 0; i < highlights.length; i += 2) {
