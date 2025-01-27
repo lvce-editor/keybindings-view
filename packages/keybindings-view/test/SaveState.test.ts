f54167058ce54f0f9d8f5021c28de5a081fa0031
Le Vivilet <levivlet@pm.me>
2025-01-27 20:17:13 +0100

    test

--- packages/keybindings-view/test/SaveState.test.ts
+++ packages/keybindings-view/test/SaveState.test.ts
@@ -6,7 +6,6 @@ test('saves state with selected tab and feature', () => {
   Create.create(1, 'test-uri', 800, 600, 0, 0, 0)
   const savedState = SaveState.saveState(1)
   expect(savedState).toEqual({
-    selectedTab: '',
-    selectedFeature: '',
+    value: '',
   })
 })
