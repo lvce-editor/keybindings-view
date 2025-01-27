5a1d2f5288c5bc5df94866ce9529d4a22ba2a776
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-27 20:05:33 +0100

    feature: add test for header virtual dom (#135)

---
+++ packages/keybindings-view/test/GetKeyBindingsTableCellEditVirtualDom.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@jest/globals'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetIconVirtualDom from '../src/parts/GetIconVirtualDom/GetIconVirtualDom.ts'
+import * as GetKeyBindingsTableCellEditVirtualDom from '../src/parts/GetKeyBindingsTableCellEditVirtualDom/GetKeyBindingsTableCellEditVirtualDom.ts'
+import * as Icon from '../src/parts/Icon/Icon.ts'
+import * as MergeClassNames from '../src/parts/MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+
+test('getKeyBindingsTableEditCellDom', () => {
+  expect(GetKeyBindingsTableCellEditVirtualDom.getKeyBindingsTableEditCellDom()).toEqual([
+    {
+      type: VirtualDomElements.Td,
+      childCount: 1,
+      className: ClassNames.KeyBindingsTableCell,
+    },
+    {
+      type: VirtualDomElements.Button,
+      className: MergeClassNames.mergeClassNames(ClassNames.IconButton, ClassNames.KeyBindingsEditButton),
+      childCount: 1,
+    },
+    GetIconVirtualDom.getIconVirtualDom(Icon.Edit),
+  ])
+})
