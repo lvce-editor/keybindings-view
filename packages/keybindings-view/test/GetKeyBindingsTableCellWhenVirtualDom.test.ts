ab3b4a7209e62d6c722d05f18829f8a5d7531961
Le Vivilet <levivlet@pm.me>
2025-01-27 20:10:32 +0100

    feature: add when test

---
+++ packages/keybindings-view/test/GetKeyBindingsTableCellWhenVirtualDom.test.ts
@@ -0,0 +1,55 @@
+import { expect, test } from '@jest/globals'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetKeyBindingsTableCellWhenVirtualDom from '../src/parts/GetKeyBindingsTableCellWhenVirtualDom/GetKeyBindingsTableCellWhenVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+
+test('getKeyBindingsTableCellWhenDom - with when condition', () => {
+  const keyBinding = {
+    when: 'editorFocus',
+  } as any
+  expect(GetKeyBindingsTableCellWhenVirtualDom.getKeyBindingsTableCellWhenDom(keyBinding)).toEqual([
+    {
+      type: VirtualDomElements.Td,
+      className: ClassNames.KeyBindingsTableCell,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'editorFocus',
+    },
+  ])
+})
+
+test('getKeyBindingsTableCellWhenDom - without when condition', () => {
+  const keyBinding = {
+    when: '',
+  } as any
+  expect(GetKeyBindingsTableCellWhenVirtualDom.getKeyBindingsTableCellWhenDom(keyBinding)).toEqual([
+    {
+      type: VirtualDomElements.Td,
+      className: ClassNames.KeyBindingsTableCell,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '',
+    },
+  ])
+})
+
+test('getKeyBindingsTableCellWhenDom - undefined when condition', () => {
+  const keyBinding = {
+    when: undefined,
+  } as any
+  expect(GetKeyBindingsTableCellWhenVirtualDom.getKeyBindingsTableCellWhenDom(keyBinding)).toEqual([
+    {
+      type: VirtualDomElements.Td,
+      className: ClassNames.KeyBindingsTableCell,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '',
+    },
+  ])
+})
