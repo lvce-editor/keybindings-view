a6eeef00dec5224aa296ae4fbb01659a7a39ca70
Le Vivilet <levivlet@pm.me>
2025-01-27 20:09:35 +0100

    feature: add key test

---
+++ packages/keybindings-view/test/GetKeyBindingsTableCellKeyVirtualDom.test.ts
@@ -0,0 +1,116 @@
+import { expect, test } from '@jest/globals'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetKeyBindingsTableCellKeyVirtualDom from '../src/parts/GetKeyBindingsTableCellKeyVirtualDom/GetKeyBindingsTableCellKeyVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+
+test('getKeyBindingsTableCellKeyDom - no modifiers', () => {
+  const keyBinding = {
+    key: 'A',
+    isCtrl: false,
+    isShift: false,
+  } as any
+  expect(GetKeyBindingsTableCellKeyVirtualDom.getKeyBindingsTableCellKeyDom(keyBinding)).toEqual([
+    {
+      type: VirtualDomElements.Td,
+      className: ClassNames.KeyBindingsTableCell,
+      childCount: 0,
+    },
+  ])
+})
+
+test('getKeyBindingsTableCellKeyDom - with ctrl', () => {
+  const keyBinding = {
+    key: 'A',
+    isCtrl: true,
+    isShift: false,
+  } as any
+  expect(GetKeyBindingsTableCellKeyVirtualDom.getKeyBindingsTableCellKeyDom(keyBinding)).toEqual([
+    {
+      type: VirtualDomElements.Td,
+      className: ClassNames.KeyBindingsTableCell,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Kbd,
+      className: ClassNames.Key,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'Ctrl',
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '+',
+    },
+  ])
+})
+
+test('getKeyBindingsTableCellKeyDom - with shift', () => {
+  const keyBinding = {
+    key: 'A',
+    isCtrl: false,
+    isShift: true,
+  } as any
+  expect(GetKeyBindingsTableCellKeyVirtualDom.getKeyBindingsTableCellKeyDom(keyBinding)).toEqual([
+    {
+      type: VirtualDomElements.Td,
+      className: ClassNames.KeyBindingsTableCell,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Kbd,
+      className: ClassNames.Key,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'Shift',
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '+',
+    },
+  ])
+})
+
+test('getKeyBindingsTableCellKeyDom - with ctrl and shift', () => {
+  const keyBinding = {
+    key: 'A',
+    isCtrl: true,
+    isShift: true,
+  } as any
+  expect(GetKeyBindingsTableCellKeyVirtualDom.getKeyBindingsTableCellKeyDom(keyBinding)).toEqual([
+    {
+      type: VirtualDomElements.Td,
+      className: ClassNames.KeyBindingsTableCell,
+      childCount: 4,
+    },
+    {
+      type: VirtualDomElements.Kbd,
+      className: ClassNames.Key,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'Ctrl',
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '+',
+    },
+    {
+      type: VirtualDomElements.Kbd,
+      className: ClassNames.Key,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'Shift',
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '+',
+    },
+  ])
+})
