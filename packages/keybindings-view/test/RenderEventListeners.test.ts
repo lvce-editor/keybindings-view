5142b17285d1059ecf1a743089944b4eb1e7e17d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-28 20:03:06 +0100

    feature: add event listener tests (#145)
    * feature: add event listener tests
    
    * lint

---
+++ packages/keybindings-view/test/RenderEventListeners.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@jest/globals'
+import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
+
+test('renderEventListeners', () => {
+  expect(RenderEventListeners.renderEventListeners()).toEqual([
+    {
+      name: DomEventListenerFunctions.HandleContextMenu,
+      params: ['handleContextMenu'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleInput,
+      params: ['handleInput', 'event.target.value'],
+    },
+    {
+      name: 'handleResizerPointerDown',
+      params: ['handleResizerClick', 'event.clientX'],
+    },
+    {
+      name: 'handleResizerMove',
+      params: ['handleResizerMove', 'event.clientY'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleContextMenu,
+      params: ['handleContextMenu', 'event.button', 'event.clientX', 'event.clientY'],
+      preventDefault: true,
+    },
+  ])
+})
