e7975c929cf574ce9ea9e23c294d956fa98494a0
Le Vivilet <levivlet@pm.me>
2025-01-28 20:00:57 +0100

    lint

--- packages/keybindings-view/test/RenderEventListeners.test.ts
+++ packages/keybindings-view/test/RenderEventListeners.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@jest/globals'
-import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 
 test('renderEventListeners', () => {
   expect(RenderEventListeners.renderEventListeners()).toEqual([
