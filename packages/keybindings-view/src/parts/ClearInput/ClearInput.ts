a606efcb37dd514cbfe2b699a9b43ee6efe19170
Le Vivilet <levivlet@pm.me>
2025-01-31 18:09:55 +0100

    lint

--- packages/keybindings-view/src/parts/ClearInput/ClearInput.ts
+++ packages/keybindings-view/src/parts/ClearInput/ClearInput.ts
@@ -1,5 +1,5 @@
-import * as HandleInput from '../HandleInput/HandleInput.ts'
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as HandleInput from '../HandleInput/HandleInput.ts'
 
 export const clearInput = (state: KeyBindingsState): KeyBindingsState => {
   return HandleInput.handleInput(state, '')
