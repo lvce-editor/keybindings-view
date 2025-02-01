7e4737cfc7e14b72fb0693ed32232c06dbd5e4b6
Le Vivilet <levivlet@pm.me>
2025-02-01 16:13:32 +0100

    feature: add keybindings actions wrapper div to allow gap between badge and buttons

--- packages/keybindings-view/src/parts/GetKeyBindingsHeaderVirtualDom/GetKeyBindingsHeaderVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetKeyBindingsHeaderVirtualDom/GetKeyBindingsHeaderVirtualDom.ts
@@ -1,17 +1,13 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import * as GetRecordingKeysBadgeVirtualDom from '../GetRecordingKeysBadgeVirtualDom/GetRecordingKeysBadgeVirtualDom.ts'
-import * as GetSearchFieldActions from '../GetSearchFieldActions/GetSearchFieldActions.ts'
-import * as GetSearchFieldButtonVirtualDom from '../GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts'
+import * as GetKeyBindingsSearchActionsVirtualDom from '../GetKeyBindingsSearchActionsVirtualDom/GetKeyBindingsSearchActionsVirtualDom.ts'
 import * as HtmlInputType from '../HtmlInputType/HtmlInputType.ts'
 import * as InputName from '../InputName/InputName.ts'
 import * as KeyBindingStrings from '../KeyBindingStrings/KeyBindingStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getKeyBindingsHeaderVirtualDom = (isRecordingKeys: boolean): readonly VirtualDomNode[] => {
-  const actions = GetSearchFieldActions.getSearchFieldActions(isRecordingKeys)
-  const childCount = isRecordingKeys ? 3 : 2
   return [
     {
       type: VirtualDomElements.Div,
@@ -21,7 +17,7 @@ export const getKeyBindingsHeaderVirtualDom = (isRecordingKeys: boolean): readon
     {
       type: VirtualDomElements.Div,
       className: ClassNames.KeyBindingsSearchWrapper,
-      childCount,
+      childCount: 2,
     },
     {
       type: VirtualDomElements.Input,
@@ -34,12 +30,6 @@ export const getKeyBindingsHeaderVirtualDom = (isRecordingKeys: boolean): readon
       childCount: 0,
       autocomplete: 'off',
     },
-    ...GetRecordingKeysBadgeVirtualDom.getRecordingKeysBadgeVirtualDom(isRecordingKeys),
-    {
-      type: VirtualDomElements.Div,
-      className: 'SearchFieldButtons',
-      childCount: actions.length,
-    },
-    ...actions.flatMap(GetSearchFieldButtonVirtualDom.getSearchFieldButtonVirtualDom),
+    ...GetKeyBindingsSearchActionsVirtualDom.getKeyBindingsSearchActionsVirtualDom(isRecordingKeys),
   ]
 }
---
+++ packages/keybindings-view/src/parts/GetKeyBindingsSearchActionsVirtualDom/GetKeyBindingsSearchActionsVirtualDom.ts
@@ -0,0 +1,25 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetRecordingKeysBadgeVirtualDom from '../GetRecordingKeysBadgeVirtualDom/GetRecordingKeysBadgeVirtualDom.ts'
+import * as GetSearchFieldActions from '../GetSearchFieldActions/GetSearchFieldActions.ts'
+import * as GetSearchFieldButtonVirtualDom from '../GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getKeyBindingsSearchActionsVirtualDom = (isRecordingKeys: boolean): readonly VirtualDomNode[] => {
+  const actions = GetSearchFieldActions.getSearchFieldActions(isRecordingKeys)
+  const childCount = isRecordingKeys ? 2 : 1
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.KeyBindingsSearchActions,
+      childCount,
+    },
+    ...GetRecordingKeysBadgeVirtualDom.getRecordingKeysBadgeVirtualDom(isRecordingKeys),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.SearchFieldButtons,
+      childCount: actions.length,
+    },
+    ...actions.flatMap(GetSearchFieldButtonVirtualDom.getSearchFieldButtonVirtualDom),
+  ]
+}
