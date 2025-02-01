11c028deb818b820dc17b9f35d5a1017ae2b5826
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-01 09:28:58 +0100

    feature: add keybindings clear e2e test (#203)

---
+++ packages/e2e/src/keybindings.clear.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'keybindings.clear'
+
+export const skip = 1
+
+export const test: Test = async ({ Main, Locator, expect, KeyBindingsEditor, Command }) => {
+  // arrange
+  await Main.openUri('app://keybindings')
+  const keyBindingsView = Locator('.Viewlet.KeyBindings')
+  await expect(keyBindingsView).toBeVisible()
+  const input = Locator('.KeyBindingsSearchInputBox')
+  await expect(input).toBeVisible()
+  await KeyBindingsEditor.handleInput('About.focus')
+  const rows = Locator('.TableBody .TableRow')
+  await expect(rows).toHaveCount(2)
+
+  // act
+  await Command.execute('KeyBindings.clearInput')
+
+  // assert
+  await expect(input).toHaveText('')
+}
