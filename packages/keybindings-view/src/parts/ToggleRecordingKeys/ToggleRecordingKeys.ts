5b1fe49d3023a05261cd3747fa5495597cfa16f7
Le Vivilet <levivlet@pm.me>
2025-02-01 09:00:43 +0100

    fix

--- packages/keybindings-view/src/parts/ToggleRecordingKeys/ToggleRecordingKeys.ts
+++ packages/keybindings-view/src/parts/ToggleRecordingKeys/ToggleRecordingKeys.ts
@@ -1,5 +1,11 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as StartRecordingKeys from '../StartRecordingKeys/StartRecordingKeys.ts'
+import * as StopRecordingKeys from '../StopRecordingKeys/StopRecordingKeys.ts'
 
 export const toggleRecordingKeys = (state: KeyBindingsState): KeyBindingsState => {
-  return state
+  const { isRecordingKeys } = state
+  if (isRecordingKeys) {
+    return StopRecordingKeys.stopRecordingKeys(state)
+  }
+  return StartRecordingKeys.startRecordingKeys(state)
 }
