62dfc905aae39454134b68158e3888edf1a84009
Le Vivilet <levivlet@pm.me>
2025-02-01 10:06:32 +0100

    feature: add recordKeys e2e test

---
+++ packages/e2e/src/keybindings.record-keys.ts
@@ -0,0 +1,28 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'keybindings.record-keys'
+
+export const skip = 1
+
+export const test: Test = async ({ Main, Locator, expect, KeyBindingsEditor, Command }) => {
+  // arrange
+  await Main.openUri('app://keybindings')
+  const keyBindingsView = Locator('.Viewlet.KeyBindings')
+  await expect(keyBindingsView).toBeVisible()
+  const input = Locator('.KeyBindingsSearchInputBox')
+  await expect(input).toBeVisible()
+  await KeyBindingsEditor.handleInput('About.focus')
+  const rows = Locator('.TableBody .TableRow')
+  await expect(rows).toHaveCount(2)
+
+  // act
+  await Command.execute('KeyBindings.startRecordingKeys')
+
+  // assert
+  const button = Locator('[name="RecordKeys"]')
+  await expect(button).toHaveClass('SearchFieldButtonChecked')
+  // TODO verify the button is marked as checked
+  // TODO verify when pressing keys, they appear in the input
+
+  // assert
+}
