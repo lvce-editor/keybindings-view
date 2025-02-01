33ac9292325d8bd9b4288df936f129787bf88a89
Le Vivilet <levivlet@pm.me>
2025-02-01 10:07:05 +0100

    feature: enable start recording keys e2e test

--- packages/e2e/src/keybindings.start-recording-keys.ts
+++ packages/e2e/src/keybindings.start-recording-keys.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'keybindings.start-recording-keys'
 
-export const skip = 1
-
 export const test: Test = async ({ Main, Locator, expect, KeyBindingsEditor, Command }) => {
   // arrange
   await Main.openUri('app://keybindings')
@@ -14,11 +12,11 @@ export const test: Test = async ({ Main, Locator, expect, KeyBindingsEditor, Com
   await KeyBindingsEditor.handleInput('About.focus')
   const rows = Locator('.TableBody .TableRow')
   await expect(rows).toHaveCount(2)
-  await Command.execute('KeyBindings.startRecordingKeys')
 
   // act
-  // TODO verify the button is marked as checked
-  // TODO verify when pressing keys, they appear in the input
+  await Command.execute('KeyBindings.startRecordingKeys')
 
   // assert
+  const button = Locator('[name="RecordKeys"]')
+  await expect(button).toHaveClass('SearchFieldButtonChecked')
 }
