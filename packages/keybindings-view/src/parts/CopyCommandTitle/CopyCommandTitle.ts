856364e3d84beb1cc305b3fb413abc59eb99a7ac
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-02 11:40:31 +0100

    feature: add more keybinding commands (#233)

---
+++ packages/keybindings-view/src/parts/CopyCommandTitle/CopyCommandTitle.ts
@@ -0,0 +1,5 @@
+import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+
+export const copyCommandTitle = (state: KeyBindingsState): KeyBindingsState => {
+  return state
+}
---
+++ packages/keybindings-view/src/parts/RemoveKeyBinding/RemoveKeyBinding.ts
@@ -0,0 +1,5 @@
+import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+
+export const removeKeyBinding = (state: KeyBindingsState): KeyBindingsState => {
+  return state
+}
