809232ec9038c52032484e837734a73c0d2f43dd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-02 11:16:31 +0100

    feature: simplify rendering (#232)
    * feature: simplify rendering
    
    * simplify
    
    * coverage

---
+++ packages/keybindings-view/src/parts/ApplyRender/ApplyRender.ts
@@ -0,0 +1,11 @@
+import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as GetRenderer from '../GetRenderer/GetRenderer.ts'
+
+export const applyRender = (oldState: KeyBindingsState, newState: KeyBindingsState, diffResult: readonly number[]): readonly any[] => {
+  const commands = []
+  for (const item of diffResult) {
+    const fn = GetRenderer.getRenderer(item)
+    commands.push(fn.apply(oldState, newState))
+  }
+  return commands
+}
