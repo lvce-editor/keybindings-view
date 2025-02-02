4ba2ffc72e63671ae150bdaac874709b8e6c05df
Le Vivilet <levivlet@pm.me>
2025-02-02 11:14:19 +0100

    simplify

--- packages/keybindings-view/src/parts/Render/Render.ts
+++ packages/keybindings-view/src/parts/Render/Render.ts
@@ -1,13 +1,8 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as Diff from '../Diff/Diff.ts'
-import * as GetRenderer from '../GetRenderer/GetRenderer.ts'
 
 export const getRenderCommands = (oldState: KeyBindingsState, newState: KeyBindingsState): readonly any[] => {
   const diffResult = Diff.diff(oldState, newState)
-  const commands = []
-  for (const item of diffResult) {
-    const fn = GetRenderer.getRenderer(item)
-    commands.push(fn.apply(oldState, newState))
-  }
-  return commands
+  return ApplyRender.applyRender(oldState, newState, diffResult)
 }
