420d0491cc9ea25aa4d9f67e3a9ff7bb422dac03
Le Vivilet <levivlet@pm.me>
2025-03-09 10:46:08 +0100

    feature: move escape key constant into separate file

--- packages/keybindings-view/src/parts/HandleKeyDown/HandleKeyDown.ts
+++ packages/keybindings-view/src/parts/HandleKeyDown/HandleKeyDown.ts
@@ -1,13 +1,14 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as HandleEscape from '../HandleEscape/HandleEscape.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
+import * as Key from '../Key/Key.ts'
 
 export const handleKeyDown = (state: KeyBindingsState, altKey: boolean, ctrlKey: boolean, key: string): KeyBindingsState => {
   const { isRecordingKeys, value } = state
   if (!isRecordingKeys) {
     return state
   }
-  if (key === 'Escape') {
+  if (key === Key.Escape) {
     return HandleEscape.handleEscape(state)
   }
   return {
