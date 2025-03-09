882ccae6645259f27646b796d3fedef1ac3162f1
Le Vivilet <levivlet@pm.me>
2025-03-09 10:39:55 +0100

    feature: add diffFocus function

--- packages/keybindings-view/src/parts/Diff/Diff.ts
+++ packages/keybindings-view/src/parts/Diff/Diff.ts
@@ -1,9 +1,10 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as DiffColumnWidths from '../DiffColumnWidths/DiffColumnWidths.ts'
+import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffKeyBindings from '../DiffKeyBindings/DiffKeyBindings.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-const modules = [DiffKeyBindings, DiffColumnWidths, DiffValue]
+const modules = [DiffKeyBindings, DiffColumnWidths, DiffValue, DiffFocus]
 
 export const diff = (oldState: KeyBindingsState, newState: KeyBindingsState): readonly number[] => {
   const diffResult: number[] = []
