3018410ba1dd14b38fdd0a04f24a9eeb92d3f816
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-28 20:02:16 +0100

    feature: rerender input value when clearing results (#247)

--- packages/keybindings-view/src/parts/ClearInput/ClearInput.ts
+++ packages/keybindings-view/src/parts/ClearInput/ClearInput.ts
@@ -1,6 +1,11 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as HandleInput from '../HandleInput/HandleInput.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
 export const clearInput = (state: KeyBindingsState): KeyBindingsState => {
-  return HandleInput.handleInput(state, '')
+  const newState = HandleInput.handleInput(state, '')
+  return {
+    ...newState,
+    inputSource: InputSource.Script,
+  }
 }
