8f3ed682e3ec8856ef042f93d49ebb32bff6902c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-28 19:55:58 +0100

    feature: don't use any type (#246)

--- packages/keybindings-view/src/parts/ScrollBarFunctions/ScrollBarFunctions.ts
+++ packages/keybindings-view/src/parts/ScrollBarFunctions/ScrollBarFunctions.ts
@@ -12,7 +12,12 @@ export const getScrollBarWidth = (width: number, longestLineWidth: number): numb
   return width ** 2 / longestLineWidth
 }
 
-export const getNewDeltaPercent = (height: number, scrollBarHeight: number, relativeY: number): any => {
+interface Result {
+  readonly percent: number
+  readonly handleOffset: number
+}
+
+export const getNewDeltaPercent = (height: number, scrollBarHeight: number, relativeY: number): Result => {
   const halfScrollBarHeight = scrollBarHeight / 2
   if (relativeY <= halfScrollBarHeight) {
     // clicked at top
