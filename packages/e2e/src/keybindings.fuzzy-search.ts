fb40a71f58774d541127267d5fbbd3c02fd2726c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-01 19:41:02 +0200

    feature: add fuzzy search test (#262)

---
+++ packages/e2e/src/keybindings.fuzzy-search.ts
@@ -0,0 +1,22 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'keybindings.fuzzy-search'
+
+export const test: Test = async ({ Main, Locator, expect, KeyBindingsEditor }) => {
+  // arrange
+  await Main.openUri('app://keybindings')
+  const keyBindingsView = Locator('.Viewlet.KeyBindings')
+  await expect(keyBindingsView).toBeVisible()
+  const input = Locator('.KeyBindingsSearchInputBox')
+  await expect(input).toBeVisible()
+
+  // act
+  await KeyBindingsEditor.handleInput('about.next')
+
+  // assert
+  const rows = Locator('.TableBody .TableRow')
+  await expect(rows).toHaveCount(1)
+  const firstRow = Locator('.TableBody .TableRow').nth(0)
+  const firstCell = firstRow.locator('.TableCell').nth(1)
+  await expect(firstCell).toHaveText('About.focusNext')
+}
