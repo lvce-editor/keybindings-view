db2d54b876edc673e79ea26a05729078b8632218
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 22:11:08 +0200

    feature: add precedence sorting (#278)
    * feature: add precedence sorting
    
    * coverage
    
    * log

--- packages/keybindings-view/src/parts/HandleInput/HandleInput.ts
+++ packages/keybindings-view/src/parts/HandleInput/HandleInput.ts
@@ -1,15 +1,17 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as FilterKeyBindings from '../FilterKeyBindings/FilterKeyBindings.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
+import * as SortKeyBindings from '../SortKeyBindings/SortKeyBindings.ts'
 
 export const handleInput = (state: KeyBindingsState, value: string): KeyBindingsState => {
-  const { parsedKeyBindings, maxVisibleItems } = state
+  const { parsedKeyBindings, maxVisibleItems, isSortingByPrecedence } = state
   const items = FilterKeyBindings.getFilteredKeyBindings(parsedKeyBindings, value)
-  const maxLineY = Math.min(items.length, maxVisibleItems)
+  const sorted = SortKeyBindings.sortKeyBindings(items, isSortingByPrecedence)
+  const maxLineY = Math.min(sorted.length, maxVisibleItems)
   return {
     ...state,
     value,
-    items,
+    items: sorted,
     maxLineY,
     inputSource: InputSource.User,
   }
--- packages/keybindings-view/src/parts/SortByPrecedence/SortByPrecedence.ts
+++ packages/keybindings-view/src/parts/SortByPrecedence/SortByPrecedence.ts
@@ -1,15 +1,13 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as SortKeyBindings from '../SortKeyBindings/SortKeyBindings.ts'
 
 export const sortByPrecedence = (state: KeyBindingsState): KeyBindingsState => {
-  const { isSortingByPrecedence } = state
-  if (isSortingByPrecedence) {
-    return {
-      ...state,
-      isSortingByPrecedence: false,
-    }
-  }
+  const { isSortingByPrecedence, items } = state
+  const newValue = !isSortingByPrecedence
+  const sorted = SortKeyBindings.sortKeyBindings(items, isSortingByPrecedence)
   return {
     ...state,
-    isSortingByPrecedence: true,
+    isSortingByPrecedence: newValue,
+    items: sorted,
   }
 }
