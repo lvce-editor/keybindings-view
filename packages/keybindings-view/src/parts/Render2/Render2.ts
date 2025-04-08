6f7596ee63e95e4ce946887f0a55182d2d004de9
Le Vivilet <levivlet@pm.me>
2025-04-08 23:08:56 +0200

    feature: remove unused render functions

--- packages/keybindings-view/src/parts/Render2/Render2.ts
+++
@@ -1,7 +0,0 @@
-import * as KeyBindingsStates from '../KeyBindingsStates/KeyBindingsStates.ts'
-import * as Render from '../Render/Render.ts'
-
-export const render2 = (uid: number): readonly any[] => {
-  const { oldState, newState } = KeyBindingsStates.get(uid)
-  return Render.getRenderCommands(oldState, newState)
-}
