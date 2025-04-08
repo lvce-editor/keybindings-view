023734b6b98204623fe5afbba5418ab13e01b0b0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 21:19:32 +0200

    feature: make wrapCommand typeSafe (#269)

--- packages/keybindings-view/src/parts/WrapCommand/WrapCommand.ts
+++ packages/keybindings-view/src/parts/WrapCommand/WrapCommand.ts
@@ -1,10 +1,23 @@
+import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as KeyBindingsStates from '../KeyBindingsStates/KeyBindingsStates.ts'
 
-export const wrapCommand = (fn: any): any => {
+export interface WrappedFn {
+  (uid: number, ...args: readonly any[]): Promise<void>
+}
+
+interface Fn {
+  (state: KeyBindingsState, ...args: readonly any[]): KeyBindingsState | Promise<KeyBindingsState>
+}
+
+export const wrapCommand = (fn: Fn): WrappedFn => {
   const wrapped = async (uid: number, ...args: readonly any[]): Promise<void> => {
     const { newState } = KeyBindingsStates.get(uid)
     const newerState = await fn(newState, ...args)
-    KeyBindingsStates.set(uid, newState, newerState)
+    if (newState === newerState) {
+      return
+    }
+    const latest = KeyBindingsStates.get(uid)
+    KeyBindingsStates.set(uid, latest.oldState, newerState)
   }
   return wrapped
 }
