fbfb96cf2d8a7859aa60253d74d20a0cf5b97350
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 21:23:24 +0200

    feature: prefix icons with maskIcon to avoid concatination later (#270)

--- packages/keybindings-view/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
@@ -1,11 +1,13 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getIconVirtualDom = (icon: string, type = VirtualDomElements.Div): VirtualDomNode => {
   return {
     type,
-    className: `MaskIcon MaskIcon${icon}`,
+    className: MergeClassNames.mergeClassNames(ClassNames.MaskIcon, icon),
     role: AriaRoles.None,
     childCount: 0,
   }
