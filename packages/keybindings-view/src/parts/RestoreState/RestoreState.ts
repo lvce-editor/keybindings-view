7cb2825e6c23c83a2cb43e3e8ea9b3bbd8140b27
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 22:05:51 +0200

    feature: include precedence in restoredState (#280)

--- packages/keybindings-view/src/parts/RestoreState/RestoreState.ts
+++ packages/keybindings-view/src/parts/RestoreState/RestoreState.ts
@@ -3,7 +3,13 @@ import * as GetSavedValue from '../GetSavedValue/GetSavedValue.ts'
 
 export const restoreState = (savedState: unknown): RestoredState => {
   const savedValue = GetSavedValue.getSavedValue(savedState)
+  // @ts-ignore
+  const isRecordingKeys = savedState?.['isRecordingKeys']
+  // @ts-ignore
+  const isSortingByPrecedence = savedState?.['isSortingByPrecedence']
   return {
     savedValue,
+    isRecordingKeys,
+    isSortingByPrecedence,
   }
 }
