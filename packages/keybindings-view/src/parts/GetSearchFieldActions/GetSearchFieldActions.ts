ff7ec2a96796a4f759a7e03ed38b3b7abcca752e
Le Vivilet <levivlet@pm.me>
2025-04-08 21:37:24 +0200

    feature: pass isSortingByPrecedence to getSearchFieldActions

--- packages/keybindings-view/src/parts/GetSearchFieldActions/GetSearchFieldActions.ts
+++ packages/keybindings-view/src/parts/GetSearchFieldActions/GetSearchFieldActions.ts
@@ -1,28 +1,28 @@
 import type { SearchFieldAction } from '../SearchFieldAction/SearchFieldAction.ts'
 import * as Icon from '../Icon/Icon.ts'
 import * as InputName from '../InputName/InputName.ts'
+import * as KeyBindingStrings from '../KeyBindingStrings/KeyBindingStrings.ts'
 
-export const getSearchFieldActions = (isRecordingKeys: boolean, value: string): readonly SearchFieldAction[] => {
+export const getSearchFieldActions = (isRecordingKeys: boolean, isSortingByPrecedence: boolean, value: string): readonly SearchFieldAction[] => {
   const hasValue = value !== ''
-  // TODO use i18n for label
   return [
     {
       name: InputName.RecordKeys,
-      label: 'RecordKeys',
+      label: KeyBindingStrings.recordKeys(),
       icon: Icon.RecordKeys,
       checked: isRecordingKeys,
       enabled: true,
     },
     {
       name: InputName.SortByPrecedence,
-      label: 'SortByPrecedence',
+      label: KeyBindingStrings.sortByPrecedence(),
       icon: Icon.SortPrecedence,
-      checked: false,
+      checked: isSortingByPrecedence,
       enabled: true,
     },
     {
       name: InputName.ClearSearchInput,
-      label: 'Clear Search Input',
+      label: KeyBindingStrings.clearAll(),
       icon: Icon.ClearAll,
       checked: false,
       enabled: hasValue,
