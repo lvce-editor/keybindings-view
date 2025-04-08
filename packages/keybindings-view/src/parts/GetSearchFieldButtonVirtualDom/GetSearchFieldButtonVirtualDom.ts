db05f5f7073611f9d89602ebd55e715560abf7b2
Le Vivilet <levivlet@pm.me>
2025-04-08 21:29:54 +0200

    fix

--- packages/keybindings-view/src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts
@@ -5,6 +5,7 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getSearchFieldClassName } from '../GetSearchFieldClassName/GetSearchFieldClassName.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
 export const getSearchFieldButtonVirtualDom = (action: SearchFieldAction): readonly VirtualDomNode[] => {
   const { name, label, icon, checked, enabled } = action
@@ -22,7 +23,7 @@ export const getSearchFieldButtonVirtualDom = (action: SearchFieldAction): reado
     },
     {
       type: VirtualDomElements.Div,
-      className: `${ClassNames.MaskIcon} MaskIcon${icon}`,
+      className: MergeClassNames.mergeClassNames(ClassNames.MaskIcon, icon),
       childCount: 0,
     },
   ]
