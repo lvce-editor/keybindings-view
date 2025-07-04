8bd80307ba03000ba2fe01e951a74e62f05daa88
Le Vivilet <levivlet@pm.me>
2025-07-04 16:46:22 +0200

    feature: rename module

--- packages/keybindings-view/src/parts/ShowDefineKeyBinding/ShowDefineKeyBinding.ts
+++ packages/keybindings-view/src/parts/ShowDefineKeyBinding/ShowDefineKeyBinding.ts
@@ -1,6 +1,6 @@
-import * as ParentRpc from '../RendererWorker/RendererWorker.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
 
 export const showDefineKeyBinding = async (): Promise<void> => {
-  await ParentRpc.invoke('Viewlet.openWidget', ViewletModuleId.DefineKeyBinding)
+  await RendererWorker.invoke('Viewlet.openWidget', ViewletModuleId.DefineKeyBinding)
 }
