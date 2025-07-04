633e012a01492aa90f112ec430de270606185696
Le Vivilet <levivlet@pm.me>
2025-07-04 16:50:17 +0200

    feature: simplify renderer worker api

--- packages/keybindings-view/src/parts/Focus/Focus.ts
+++ packages/keybindings-view/src/parts/Focus/Focus.ts
@@ -1,5 +1,3 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
-export const setFocus = (focusKey: number): Promise<void> => {
-  return RendererWorker.invoke('Focus.setFocus', focusKey)
-}
+export const setFocus = RendererWorker.setFocus
