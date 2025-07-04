20102ed69037c6330584c5fedf7d9b74ad0d5bcc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 16:57:05 +0200

    feature: simplify renderer worker api (#305)
    * feature: simplify renderer worker api
    
    * lint

--- packages/keybindings-view/src/parts/RendererWorker/RendererWorker.ts
+++ packages/keybindings-view/src/parts/RendererWorker/RendererWorker.ts
@@ -1,3 +1,3 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const { invoke, set } = RendererWorker
+export const { invoke, set, writeClipBoardText, setFocus, showContextMenu, openWidget } = RendererWorker
