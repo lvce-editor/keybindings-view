4977a67bf96e23434d2f6e28f971d0be4f5ee846
Le Vivilet <levivlet@pm.me>
2025-08-27 10:10:16 +0200

    feature: update value rendering

--- packages/keybindings-view/src/parts/GetRenderer/RenderValue.ts
+++ packages/keybindings-view/src/parts/GetRenderer/RenderValue.ts
@@ -1,6 +1,7 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const renderValue = (oldState: KeyBindingsState, newState: KeyBindingsState): readonly any[] => {
   const { uid, value } = newState
-  return [/* method */ 'setValue', uid, /* setValue */ value]
+  return ['Viewlet.setValueByName', uid, InputName.KeyBindingsFilter, /* setValue */ value]
 }
