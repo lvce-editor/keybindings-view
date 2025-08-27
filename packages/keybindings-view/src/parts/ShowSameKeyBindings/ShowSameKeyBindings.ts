8ecdba7e4b6fc765f6fd24fb6634efba232d9268
Le Vivilet <levivlet@pm.me>
2025-08-27 11:48:59 +0200

    feature: add showSameKeyBindings

---
+++ packages/keybindings-view/src/parts/ShowSameKeyBindings/ShowSameKeyBindings.ts
@@ -0,0 +1,5 @@
+import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+
+export const showSameKeyBindings = async (state: KeyBindingsState): Promise<KeyBindingsState> => {
+  return state
+}
