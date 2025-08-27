8ed3e61a32d97603cf07270d008e63dfd159ac60
Le Vivilet <levivlet@pm.me>
2025-08-27 13:11:29 +0200

    update

--- packages/keybindings-view/src/parts/FilterKeyBindings/FilterKeyBindings.ts
+++ packages/keybindings-view/src/parts/FilterKeyBindings/FilterKeyBindings.ts
@@ -1,5 +1,5 @@
-import * as FilterKeyBinding from '../FilterKeyBinding/FilterKeyBinding.ts'
-import * as GetKeyModifierPrefix from '../GetKeyModifierPrefix/GetKeyModifierPrefix.ts'
+import * as FilterKeyBindingsDefault from '../FilterKeyBindingsDefault/FilterKeyBindingsDefault.ts'
+import * as FilterKeyBindingsByKeyBinding from '../FilterKeyBindingsByKeyBinding/FilterKeyBindingsByKeyBinding.ts'
 import * as WithEmptyMatches from '../WithEmptyMatches/WithEmptyMatches.ts'
 
 export const getFilteredKeyBindings = (keyBindings: readonly any[], value: string): readonly any[] => {
@@ -9,45 +9,7 @@ export const getFilteredKeyBindings = (keyBindings: readonly any[], value: strin
   // Exact match syntax: quoted string means search for exact keybinding
   const isQuoted = value.length >= 2 && value.startsWith('"') && value.endsWith('"')
   if (isQuoted) {
-    return filterKeyBindingsByKeyBinding(keyBindings, value)
+    return FilterKeyBindingsByKeyBinding.filterKeyBindingsByKeyBinding(keyBindings, value)
   }
-  return filterKeyBindingsDefault(keyBindings, value)
-}
-
-export const filterKeyBindingsDefault = (keyBindings: readonly any[], value: string): readonly any[] => {
-  const filteredKeyBindings = []
-  for (const keyBinding of keyBindings) {
-    const { command, key } = keyBinding
-    const commandMatches = FilterKeyBinding.filterKeyBinding(value, command)
-    const keyMatches = FilterKeyBinding.filterKeyBinding(value, key)
-    if (commandMatches.length > 0 || keyMatches.length > 0) {
-      filteredKeyBindings.push({
-        ...keyBinding,
-        commandMatches,
-        keyMatches,
-      })
-    }
-  }
-  return filteredKeyBindings
-}
-
-export const filterKeyBindingsByKeyBinding = (keyBindings: readonly any[], value: string): readonly any[] => {
-  const exact = value.slice(1, -1)
-  const filteredKeyBindings = []
-  for (const keyBinding of keyBindings) {
-    const { key } = keyBinding
-    const isCtrl = Boolean((keyBinding as any).isCtrl)
-    const isShift = Boolean((keyBinding as any).isShift)
-    const prefix = GetKeyModifierPrefix.getKeyModifierPrefix(false, isCtrl, isShift, false)
-    const combinedUnspaced = `${prefix}${key}`
-    const combinedSpaced = combinedUnspaced.replaceAll('+', ' + ')
-    if (combinedSpaced === exact || key === exact) {
-      filteredKeyBindings.push({
-        ...keyBinding,
-        commandMatches: [],
-        keyMatches: [],
-      })
-    }
-  }
-  return filteredKeyBindings
+  return FilterKeyBindingsDefault.filterKeyBindingsDefault(keyBindings, value)
 }
---
+++ packages/keybindings-view/test/FilterKeyBindingsDefault.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from '@jest/globals'
+import * as FilterKeyBindingsDefault from '../src/parts/FilterKeyBindingsDefault/FilterKeyBindingsDefault.ts'
+
+test('filterKeyBindingsDefault - fuzzy command match', () => {
+	const keyBindings = [
+		{ command: 'EditorCompletion.focusNext', key: 'ArrowDown' },
+		{ command: 'EditorCompletion.focusPrevious', key: 'ArrowUp' },
+	]
+	const value = 'fpr'
+	expect(FilterKeyBindingsDefault.filterKeyBindingsDefault(keyBindings, value)).toEqual([
+		{
+			command: 'EditorCompletion.focusPrevious',
+			key: 'ArrowUp',
+			commandMatches: [26, 17, 18, 22, 24],
+			keyMatches: [],
+		},
+	])
+})
+
+
