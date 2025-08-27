bde485723c3ea4dce4bf9f82360516cb55f26a82
Le Vivilet <levivlet@pm.me>
2025-08-27 13:44:55 +0200

    lint

--- packages/keybindings-view/test/FilterKeyBindingsByKeyBinding.test.ts
+++ packages/keybindings-view/test/FilterKeyBindingsByKeyBinding.test.ts
@@ -2,19 +2,19 @@ import { expect, test } from '@jest/globals'
 import * as FilterKeyBindingsByKeyBinding from '../src/parts/FilterKeyBindingsByKeyBinding/FilterKeyBindingsByKeyBinding.ts'
 
 test('filterKeyBindingsByKeyBinding - exact quoted key match', () => {
-	const keyBindings = [
-		{ command: 'EditorCompletion.focusNext', key: 'Ctrl + Space', isCtrl: true, isShift: false },
-		{ command: 'EditorCompletion.focusPrevious', key: 'Ctrl + ArrowUp', isCtrl: true, isShift: false },
-	]
-	const value = '"Ctrl + Space"'
-	expect(FilterKeyBindingsByKeyBinding.filterKeyBindingsByKeyBinding(keyBindings, value)).toEqual([
-		{
-			command: 'EditorCompletion.focusNext',
-			key: 'Ctrl + Space',
-			isCtrl: true,
-			isShift: false,
-			keyMatches: [],
-			commandMatches: [],
-		},
-	])
+  const keyBindings = [
+    { command: 'EditorCompletion.focusNext', key: 'Ctrl + Space', isCtrl: true, isShift: false },
+    { command: 'EditorCompletion.focusPrevious', key: 'Ctrl + ArrowUp', isCtrl: true, isShift: false },
+  ]
+  const value = '"Ctrl + Space"'
+  expect(FilterKeyBindingsByKeyBinding.filterKeyBindingsByKeyBinding(keyBindings, value)).toEqual([
+    {
+      command: 'EditorCompletion.focusNext',
+      key: 'Ctrl + Space',
+      isCtrl: true,
+      isShift: false,
+      keyMatches: [],
+      commandMatches: [],
+    },
+  ])
 })
