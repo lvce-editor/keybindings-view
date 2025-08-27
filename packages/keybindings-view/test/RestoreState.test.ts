2427e5be028df460781a9766c63e5bb024160c0e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-27 17:01:31 +0200

    feature: restore focus (#371)

---
+++ packages/keybindings-view/test/RestoreState.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@jest/globals'
+import * as RestoreState from '../src/parts/RestoreState/RestoreState.ts'
+
+test('restoreState - restores value, flags and focus', () => {
+  const savedState = {
+    value: 'abc',
+    isRecordingKeys: true,
+    isSortingByPrecedence: true,
+    focus: 2,
+  }
+  const restored = RestoreState.restoreState(savedState)
+  expect(restored).toEqual({
+    savedValue: 'abc',
+    isRecordingKeys: true,
+    isSortingByPrecedence: true,
+    focus: 2,
+  })
+})
