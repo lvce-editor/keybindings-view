6c23b85c02ab2bc320887c4b9045299313bb3233
Le Vivilet <levivlet@pm.me>
2025-08-27 09:47:15 +0200

    feature: pass uid when rendering

--- packages/keybindings-view/src/parts/GetRenderer/RenderFocus.ts
+++ packages/keybindings-view/src/parts/GetRenderer/RenderFocus.ts
@@ -2,5 +2,5 @@ import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const renderFocus = (oldState: KeyBindingsState, newState: KeyBindingsState): any => {
-  return ['Viewlet.focusSelector', InputName.KeyBindingsFilter]
+  return ['Viewlet.focusSelector', newState.uid, InputName.KeyBindingsFilter]
 }
