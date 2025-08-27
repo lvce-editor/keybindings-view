57d659232bd7809fe750242927de732b04b4643d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-27 13:46:01 +0200

    feature: update keybindings filtering to allow filtering by exact key… (#351)

---
+++ packages/keybindings-view/src/parts/FilterKeyBindingsByKeyBinding/FilterKeyBindingsByKeyBinding.ts
@@ -0,0 +1,22 @@
+import * as GetKeyModifierPrefix from '../GetKeyModifierPrefix/GetKeyModifierPrefix.ts'
+
+export const filterKeyBindingsByKeyBinding = (keyBindings: readonly any[], value: string): readonly any[] => {
+  const exact = value.slice(1, -1)
+  const filteredKeyBindings = []
+  for (const keyBinding of keyBindings) {
+    const { key } = keyBinding
+    const isCtrl = Boolean(keyBinding.isCtrl)
+    const isShift = Boolean(keyBinding.isShift)
+    const prefix = GetKeyModifierPrefix.getKeyModifierPrefix(false, isCtrl, isShift, false)
+    const combinedUnspaced = `${prefix}${key}`
+    const combinedSpaced = combinedUnspaced.replaceAll('+', ' + ')
+    if (combinedSpaced === exact || key === exact) {
+      filteredKeyBindings.push({
+        ...keyBinding,
+        commandMatches: [],
+        keyMatches: [],
+      })
+    }
+  }
+  return filteredKeyBindings
+}
---
+++ packages/keybindings-view/src/parts/FilterKeyBindingsDefault/FilterKeyBindingsDefault.ts
@@ -0,0 +1,18 @@
+import * as FilterKeyBinding from '../FilterKeyBinding/FilterKeyBinding.ts'
+
+export const filterKeyBindingsDefault = (keyBindings: readonly any[], value: string): readonly any[] => {
+  const filteredKeyBindings = []
+  for (const keyBinding of keyBindings) {
+    const { command, key } = keyBinding
+    const commandMatches = FilterKeyBinding.filterKeyBinding(value, command)
+    const keyMatches = FilterKeyBinding.filterKeyBinding(value, key)
+    if (commandMatches.length > 0 || keyMatches.length > 0) {
+      filteredKeyBindings.push({
+        ...keyBinding,
+        commandMatches,
+        keyMatches,
+      })
+    }
+  }
+  return filteredKeyBindings
+}
