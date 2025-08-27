2e5bde31ed3e18f1db9dfe6c376b248d431c63bf
Le Vivilet <levivlet@pm.me>
2025-08-27 10:05:48 +0200

    lint

--- packages/keybindings-view/src/parts/VirtualDomElements/VirtualDomElements.ts
+++ packages/keybindings-view/src/parts/VirtualDomElements/VirtualDomElements.ts
@@ -1,16 +1,16 @@
 import { VirtualDomElements as WorkerVirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 
-export const {Button} = WorkerVirtualDomElements
-export const {Col} = WorkerVirtualDomElements
-export const {ColGroup} = WorkerVirtualDomElements
-export const {Div} = WorkerVirtualDomElements
-export const {Input} = WorkerVirtualDomElements
-export const {Kbd} = WorkerVirtualDomElements
-export const {Span} = WorkerVirtualDomElements
-export const {Table} = WorkerVirtualDomElements
-export const {TBody} = WorkerVirtualDomElements
-export const {Td} = WorkerVirtualDomElements
-export const {Text} = WorkerVirtualDomElements
-export const {Th} = WorkerVirtualDomElements
-export const {THead} = WorkerVirtualDomElements
-export const {Tr} = WorkerVirtualDomElements
+export const { Button } = WorkerVirtualDomElements
+export const { Col } = WorkerVirtualDomElements
+export const { ColGroup } = WorkerVirtualDomElements
+export const { Div } = WorkerVirtualDomElements
+export const { Input } = WorkerVirtualDomElements
+export const { Kbd } = WorkerVirtualDomElements
+export const { Span } = WorkerVirtualDomElements
+export const { Table } = WorkerVirtualDomElements
+export const { TBody } = WorkerVirtualDomElements
+export const { Td } = WorkerVirtualDomElements
+export const { Text } = WorkerVirtualDomElements
+export const { Th } = WorkerVirtualDomElements
+export const { THead } = WorkerVirtualDomElements
+export const { Tr } = WorkerVirtualDomElements
