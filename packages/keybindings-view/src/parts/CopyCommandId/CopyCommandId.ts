f14e161d97db0792b668bb893879c8439cbba19e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-27 11:41:27 +0200

    feature: implement copyCommandId (#337)

--- packages/keybindings-view/src/parts/CopyCommandId/CopyCommandId.ts
+++ packages/keybindings-view/src/parts/CopyCommandId/CopyCommandId.ts
@@ -1,5 +1,13 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
-export const copyCommandId = (state: KeyBindingsState): KeyBindingsState => {
+export const copyCommandId = async (state: KeyBindingsState): Promise<KeyBindingsState> => {
+  const { focusedIndex, items } = state
+  const item = items[focusedIndex]
+  if (!item) {
+    return state
+  }
+  const { command } = item
+  await RendererWorker.writeClipBoardText(command)
   return state
 }
