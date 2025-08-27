975dfc5c2206147641a9ac5e34c2dad5daa952b6
Le Vivilet <levivlet@pm.me>
2025-08-27 10:02:56 +0200

    feature: use dom elements from worker dom

--- packages/keybindings-view/src/parts/VirtualDomElements/VirtualDomElements.ts
+++ packages/keybindings-view/src/parts/VirtualDomElements/VirtualDomElements.ts
@@ -1,14 +1,16 @@
-export const Button = 1
-export const Col = 2
-export const ColGroup = 3
-export const Div = 4
-export const Input = 6
-export const Kbd = 7
-export const Span = 8
-export const Table = 9
-export const TBody = 10
-export const Td = 11
-export const Text = 12
-export const Th = 13
-export const THead = 14
-export const Tr = 15
+import { VirtualDomElements as WorkerVirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+
+export const Button = WorkerVirtualDomElements.Button
+export const Col = WorkerVirtualDomElements.Col
+export const ColGroup = WorkerVirtualDomElements.ColGroup
+export const Div = WorkerVirtualDomElements.Div
+export const Input = WorkerVirtualDomElements.Input
+export const Kbd = WorkerVirtualDomElements.Kbd
+export const Span = WorkerVirtualDomElements.Span
+export const Table = WorkerVirtualDomElements.Table
+export const TBody = WorkerVirtualDomElements.TBody
+export const Td = WorkerVirtualDomElements.Td
+export const Text = WorkerVirtualDomElements.Text
+export const Th = WorkerVirtualDomElements.Th
+export const THead = WorkerVirtualDomElements.THead
+export const Tr = WorkerVirtualDomElements.Tr
