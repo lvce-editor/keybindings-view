5e30f622e2a830eb077928f8d819cfbd57359e49
Le Vivilet <levivlet@pm.me>
2025-08-27 10:24:47 +0200

    fix: race condition in rendering when executing two commands at the same time

--- packages/keybindings-view/src/parts/Render3/Render3.ts
+++ packages/keybindings-view/src/parts/Render3/Render3.ts
@@ -2,6 +2,9 @@ import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as KeyBindingsStates from '../KeyBindingsStates/KeyBindingsStates.ts'
 
 export const render3 = (uid: number, diffResult: readonly number[]): readonly any[] => {
+  if (diffResult.length === 0) {
+    return []
+  }
   const { oldState, newState } = KeyBindingsStates.get(uid)
   KeyBindingsStates.set(uid, newState, newState)
   const commands = ApplyRender.applyRender(oldState, newState, diffResult)
