b2f64d7304fff053318110949bf4f5f342abd485
Le Vivilet <levivlet@pm.me>
2025-08-27 14:01:12 +0200

    feature: add handleInputFocus

---
+++ packages/keybindings-view/src/parts/HandleInputFocus/HandleInputFocus.ts
@@ -0,0 +1,11 @@
+import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as FocusKey from '../FocusKey/FocusKey.ts'
+
+export const handleInputFocus = (state: KeyBindingsState): KeyBindingsState => {
+  return {
+    ...state,
+    focus: FocusKey.Input,
+  }
+}
+
+
