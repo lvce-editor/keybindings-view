2d77323d1a27a29dde695fec7569ba5db4d99f99
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-27 09:26:19 +0200

    feature: add uid property (#320)

--- packages/keybindings-view/src/parts/Create/Create.ts
+++ packages/keybindings-view/src/parts/Create/Create.ts
@@ -36,6 +36,7 @@ export const create = (uid: number, uri: string, x: number, y: number, width: nu
     inputSource: InputSource.User,
     isSortingByPrecedence: false,
     headerHeight: 0,
+    uid,
   }
   KeyBindingsStates.set(uid, state, state)
 }
