9d15e218ae2ab83840fdf5e58a3591a0644628ec
Le Vivilet <levivlet@pm.me>
2025-08-27 11:56:47 +0200

    feature: add showSameKeyBindings e2e test

---
+++ packages/e2e/src/keybindings.show-same-keybindings.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'keybindings.show-same-keybindings'
+
+export const skip = 1
+
+export const test: Test = async ({ Main, Locator, expect, KeyBindingsEditor }) => {
+  // arrange
+  await Main.openUri('app://keybindings')
+  const keyBindingsView = Locator('.Viewlet.KeyBindings')
+  await expect(keyBindingsView).toBeVisible()
+  const input = Locator('.KeyBindingsSearchInputBox')
+  await expect(input).toBeVisible()
+  await KeyBindingsEditor.handleInput('About.focus')
+  const rows = Locator('.TableBody .TableRow')
+  await expect(rows).toHaveCount(2)
+  await KeyBindingsEditor.focusFirst()
+
+  // act
+  await KeyBindingsEditor.showSameKeyBindings()
+
+  // assert
+}
