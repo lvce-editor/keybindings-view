063f3958d0c637f2afbacbc2523d41c331ab9c6e
Le Vivilet <levivlet@pm.me>
2025-08-27 09:27:08 +0200

    feature: move renderer functions into separate files

---
+++ packages/keybindings-view/src/parts/GetRenderer/RenderFocus.ts
@@ -0,0 +1,8 @@
+import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as InputName from '../InputName/InputName.ts'
+
+export const renderFocus = (oldState: KeyBindingsState, newState: KeyBindingsState): any => {
+  return ['Viewlet.focusSelector', InputName.KeyBindingsFilter]
+}
+
+
---
+++ packages/keybindings-view/src/parts/GetRenderer/RenderValue.ts
@@ -0,0 +1,7 @@
+import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+
+export const renderValue = (oldState: KeyBindingsState, newState: KeyBindingsState): any => {
+  return [/* method */ 'setValue', /* setValue */ newState.value]
+}
+
+
