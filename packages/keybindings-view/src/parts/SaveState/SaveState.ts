ef320941488ac1547721b83a8984fbf3b7b404d2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-27 17:09:57 +0200

    feature: save selectedIndex (#377)

--- packages/keybindings-view/src/parts/SaveState/SaveState.ts
+++ packages/keybindings-view/src/parts/SaveState/SaveState.ts
@@ -1,11 +1,12 @@
 import type { SavedState } from '../SavedState/SavedState.ts'
 
 export const saveState = (state: SavedState): SavedState => {
-  const { value, isRecordingKeys, isSortingByPrecedence, focus } = state
+  const { value, isRecordingKeys, isSortingByPrecedence, focus, selectedIndex } = state
   return {
     value,
     isRecordingKeys,
     isSortingByPrecedence,
     focus,
+    selectedIndex,
   }
 }
