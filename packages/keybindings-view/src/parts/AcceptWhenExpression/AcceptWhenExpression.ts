9026806364e0a6689ad05a7de67a3ca843d6928c
Le Vivilet <levivlet@pm.me>
2025-08-28 18:27:23 +0200

    feature: update visible keybindings when changing when expression

--- packages/keybindings-view/src/parts/AcceptWhenExpression/AcceptWhenExpression.ts
+++ packages/keybindings-view/src/parts/AcceptWhenExpression/AcceptWhenExpression.ts
@@ -1,8 +1,12 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import { getVisibleKeyBindings } from '../GetVisibleKeyBindings/GetVisibleKeyBindings.ts'
 
 export const acceptWhenExpression = (state: KeyBindingsState): KeyBindingsState => {
+  const { items, minLineY, maxLineY, selectedIndex } = state
+  const visibleItems = getVisibleKeyBindings(items, minLineY, maxLineY, selectedIndex, false)
   return {
     ...state,
     editingWhenExpression: false,
+    visibleItems,
   }
 }
--- packages/keybindings-view/src/parts/ChangeWhenExpression/ChangeWhenExpression.ts
+++ packages/keybindings-view/src/parts/ChangeWhenExpression/ChangeWhenExpression.ts
@@ -1,18 +1,21 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import { getVisibleKeyBindings } from '../GetVisibleKeyBindings/GetVisibleKeyBindings.ts'
 import * as GetWhenExpressionText from '../GetWhenExpressionText/GetWhenExpressionText.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const changeWhenExpression = (state: KeyBindingsState): KeyBindingsState => {
-  const { items, selectedIndex } = state
+  const { items, selectedIndex, minLineY, maxLineY } = state
   const selectedItem = items[selectedIndex]
   if (!selectedItem) {
     return state
   }
   const text = GetWhenExpressionText.getWhenExpressionText(selectedItem.when)
+  const visibleItems = getVisibleKeyBindings(items, minLineY, maxLineY, selectedIndex, true)
   return {
     ...state,
     editingWhenExpression: true,
     focus: WhenExpression.FocusKeyBindingsWhenExpression,
     whenExpressionText: text,
+    visibleItems,
   }
 }
