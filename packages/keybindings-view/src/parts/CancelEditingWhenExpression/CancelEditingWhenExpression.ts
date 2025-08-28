878e7f053b74d8148a8b311f8746be1746489078
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-28 18:30:53 +0200

    feature: update visible keybindings when changing when expression (#467)

--- packages/keybindings-view/src/parts/CancelEditingWhenExpression/CancelEditingWhenExpression.ts
+++ packages/keybindings-view/src/parts/CancelEditingWhenExpression/CancelEditingWhenExpression.ts
@@ -1,8 +1,12 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import { getVisibleKeyBindings } from '../GetVisibleKeyBindings/GetVisibleKeyBindings.ts'
 
 export const cancelEditingWhenExpression = (state: KeyBindingsState): KeyBindingsState => {
+  const { items, minLineY, maxLineY, selectedIndex } = state
+  const visibleItems = getVisibleKeyBindings(items, minLineY, maxLineY, selectedIndex, true)
   return {
     ...state,
     editingWhenExpression: false,
+    visibleItems,
   }
 }
