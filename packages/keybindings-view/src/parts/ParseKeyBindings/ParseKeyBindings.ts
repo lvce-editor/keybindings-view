0108843cdc10e9baf1c3719bbc591121828149e0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-28 15:58:08 +0200

    Discard changes to packages/keybindings-view/src/parts/ParseKeyBindings/ParseKeyBindings.ts

--- packages/keybindings-view/src/parts/ParseKeyBindings/ParseKeyBindings.ts
+++ packages/keybindings-view/src/parts/ParseKeyBindings/ParseKeyBindings.ts
@@ -1,6 +1,5 @@
-import type { ParsedKeyBinding } from '../ParsedKeyBinding/ParsedKeyBinding.ts'
 import * as ParseKeyBinding from '../ParseKeyBinding/ParseKeyBinding.ts'
 
-export const parseKeyBindings = (keyBindings: readonly any[]): readonly ParsedKeyBinding[] => {
+export const parseKeyBindings = (keyBindings: readonly any[]): readonly any[] => {
   return keyBindings.map(ParseKeyBinding.parseKeyBinding)
 }
