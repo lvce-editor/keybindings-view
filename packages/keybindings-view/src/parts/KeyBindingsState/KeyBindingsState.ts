34fdac7ac48a87c385e91f7eab4971574221e7a4
Le Vivilet <levivlet@pm.me>
2025-08-28 12:13:12 +0200

    feature: add whenExpression text property

--- packages/keybindings-view/src/parts/KeyBindingsState/KeyBindingsState.ts
+++ packages/keybindings-view/src/parts/KeyBindingsState/KeyBindingsState.ts
@@ -1,32 +1,33 @@
 import type { List } from '../List/List.ts'
 
 export interface KeyBindingsState extends List<any> {
-  readonly parsedKeyBindings: readonly any[]
-  readonly items: readonly any[]
-  readonly maxVisibleItems: number
-  readonly x: number
-  readonly y: number
-  readonly width: number
-  readonly value: string
-  readonly selectedIndex: number
-  readonly finalDeltaY: number
-  readonly deltaY: number
-  readonly uri: string
   readonly columnWidth1: number
   readonly columnWidth2: number
   readonly columnWidth3: number
   readonly contentPadding: number
-  readonly resizerDownId: number
   readonly defineKeyBindingsId: number
+  readonly deltaY: number
   readonly editIconSize: number
-  readonly padding: number
-  readonly searchHeaderHeight: number
-  readonly tableHeaderHeight: number
-  readonly minimumSliderSize: number
+  readonly editingWhenExpression: boolean
+  readonly finalDeltaY: number
+  readonly focus: number
+  readonly inputSource: number
   readonly isRecordingKeys: boolean
   readonly isSortingByPrecedence: boolean
-  readonly inputSource: number
+  readonly items: readonly any[]
+  readonly maxVisibleItems: number
+  readonly minimumSliderSize: number
+  readonly padding: number
+  readonly parsedKeyBindings: readonly any[]
+  readonly resizerDownId: number
+  readonly searchHeaderHeight: number
+  readonly selectedIndex: number
+  readonly tableHeaderHeight: number
   readonly uid: number
-  readonly focus: number
-  readonly editingWhenExpression: boolean
+  readonly uri: string
+  readonly value: string
+  readonly whenExpressionText: string
+  readonly width: number
+  readonly x: number
+  readonly y: number
 }
