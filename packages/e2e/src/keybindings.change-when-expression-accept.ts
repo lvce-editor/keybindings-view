13ee307977ed74d1179f7f2a653e2608c2a2a8fe
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-28 12:00:18 +0200

    feature: add more when expression e2e tests (#404)

---
+++ packages/e2e/src/keybindings.change-when-expression-accept.ts
@@ -0,0 +1,18 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'keybindings.change-when-expression-accept'
+
+export const skip = 1
+
+export const test: Test = async ({ KeyBindingsEditor }) => {
+  // arrange
+  await KeyBindingsEditor.open()
+  await KeyBindingsEditor.handleInput('About.focus')
+  await KeyBindingsEditor.focusFirst()
+
+  // act
+  // await KeyBindingsEditor.changeWhenExpression()
+
+  // assert
+  // await expect(input).toHaveText('')
+}
