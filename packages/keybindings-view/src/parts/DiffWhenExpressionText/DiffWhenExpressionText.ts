b4787ce17ef152b959035082fb96b7d614f33d21
Le Vivilet <levivlet@pm.me>
2025-08-28 12:12:03 +0200

    feature: improve whenExpression diff

--- packages/keybindings-view/src/parts/DiffWhenExpressionText/DiffWhenExpressionText.ts
+++ packages/keybindings-view/src/parts/DiffWhenExpressionText/DiffWhenExpressionText.ts
@@ -1,5 +1,5 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 
 export const isEqual = (oldState: KeyBindingsState, newState: KeyBindingsState): boolean => {
-  return oldState.editingWhenExpression === false && newState.editingWhenExpression === true
+  return oldState.editingWhenExpression === true || newState.editingWhenExpression === false
 }
