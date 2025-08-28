cba4715bca4fbb3e35a2b7f2a68aeb54a06dc6ee
Le Vivilet <levivlet@pm.me>
2025-08-28 09:18:57 +0200

    feature: set editingWhenExpression to true on changeWhenExpression

--- packages/keybindings-view/src/parts/ChangeWhenExpression/ChangeWhenExpression.ts
+++ packages/keybindings-view/src/parts/ChangeWhenExpression/ChangeWhenExpression.ts
@@ -1,5 +1,8 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 
 export const changeWhenExpression = (state: KeyBindingsState): KeyBindingsState => {
-  return state
+  return {
+    ...state,
+    editingWhenExpression: true,
+  }
 }
