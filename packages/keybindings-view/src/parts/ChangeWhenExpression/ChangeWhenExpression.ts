659f09890c352ecd81e5bf25325f80ccfeae10a1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-28 12:18:04 +0200

    feature: update when expression text when entering edit mode (#412)

--- packages/keybindings-view/src/parts/ChangeWhenExpression/ChangeWhenExpression.ts
+++ packages/keybindings-view/src/parts/ChangeWhenExpression/ChangeWhenExpression.ts
@@ -1,10 +1,18 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as GetWhenExpressionText from '../GetWhenExpressionText/GetWhenExpressionText.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const changeWhenExpression = (state: KeyBindingsState): KeyBindingsState => {
+  const { items, selectedIndex } = state
+  const selectedItem = items[selectedIndex]
+  if (!selectedItem) {
+    return state
+  }
+  const text = GetWhenExpressionText.getWhenExpressionText(selectedItem.when)
   return {
     ...state,
     editingWhenExpression: true,
     focus: WhenExpression.FocusKeyBindingsWhenExpression,
+    whenExpressionText: text,
   }
 }
