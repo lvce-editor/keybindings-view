fbf59ff87eb740a6172ae607309509d817d4e640
Le Vivilet <levivlet@pm.me>
2025-08-28 18:43:58 +0200

    focus

--- packages/keybindings-view/src/parts/ListFocusIndexScrollUp/ListFocusIndexScrollUp.ts
+++ packages/keybindings-view/src/parts/ListFocusIndexScrollUp/ListFocusIndexScrollUp.ts
@@ -1,17 +1,19 @@
-import type { List } from '../List/List.ts'
 import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.ts'
+import { getVisibleKeyBindings } from '../GetVisibleKeyBindings/GetVisibleKeyBindings.ts'
+import { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 
-export const focusIndexScrollUp = <T, State extends List<T>>(
-  state: State,
+export const focusIndexScrollUp = (
+  state: KeyBindingsState,
   index: number,
   listHeight: number,
   itemHeight: number,
   itemsLength: number,
-): State => {
+): KeyBindingsState => {
   const newMinLineY = index
   const fittingItems = GetNumberOfVisibleItems.getNumberOfVisibleItems(listHeight, itemHeight)
   const newMaxLineY = Math.min(newMinLineY + fittingItems, itemsLength)
   const newDeltaY = newMinLineY * itemHeight
+  const visibleItems = getVisibleKeyBindings(state.items, newMinLineY, newMaxLineY, state.selectedIndex, state.editingWhenExpression)
   return {
     ...state,
     focusedIndex: index,
@@ -20,5 +22,6 @@ export const focusIndexScrollUp = <T, State extends List<T>>(
     focused: true,
     deltaY: newDeltaY,
     selectedIndex: index,
+    visibleItems,
   }
 }
