1edb41241a14a68a3e2d9973c6b78990ba6392fe
Le Vivilet <levivlet@pm.me>
2025-08-28 18:07:51 +0200

    feature: move input dom into separate file

---
+++ packages/keybindings-view/src/parts/GetKeyBindingsInputVirtualDom/GetKeyBindingsInputVirtualDom.ts
@@ -0,0 +1,24 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as GetKeyBindingsSearchActionsVirtualDom from '../GetKeyBindingsSearchActionsVirtualDom/GetKeyBindingsSearchActionsVirtualDom.ts'
+import * as HtmlInputType from '../HtmlInputType/HtmlInputType.ts'
+import * as InputName from '../InputName/InputName.ts'
+import * as KeyBindingStrings from '../KeyBindingStrings/KeyBindingStrings.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getKeyBindingsInputVirtualDom = (): VirtualDomNode => {
+  return {
+    type: VirtualDomElements.Input,
+    className: ClassNames.KeyBindingsSearchInputBox,
+    inputType: HtmlInputType.Search,
+    placeholder: KeyBindingStrings.typeToSearchKeyBindings(),
+    name: InputName.KeyBindingsFilter,
+    onFocus: DomEventListenerFunctions.HandleInputFocus,
+    onInput: DomEventListenerFunctions.HandleInput,
+    onKeyDown: DomEventListenerFunctions.HandleKeyDown,
+    ariaDescription: KeyBindingStrings.resultsWillUpdateAsYouType(),
+    childCount: 0,
+    autocomplete: 'off',
+  }
+}
