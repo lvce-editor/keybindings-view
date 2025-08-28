f5710ba437faa8072dc8412130f8523bdf809a16
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-28 18:18:03 +0200

    feature: update visible keybindings on setDeltaY (#464)

--- packages/keybindings-view/src/parts/SetDeltaY/SetDeltaY.ts
+++ packages/keybindings-view/src/parts/SetDeltaY/SetDeltaY.ts
@@ -1,7 +1,8 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import { getVisibleKeyBindings } from '../GetVisibleKeyBindings/GetVisibleKeyBindings.ts'
 
 export const setDeltaY = (state: KeyBindingsState, deltaY: number): KeyBindingsState => {
-  const { maxVisibleItems, itemHeight, items } = state
+  const { maxVisibleItems, itemHeight, items, editingWhenExpression, selectedIndex } = state
   const tableHeight = maxVisibleItems * itemHeight
   const minDeltaY = 0
   const maxDeltaY = Math.max(items.length * itemHeight - tableHeight, 0)
@@ -12,10 +13,12 @@ export const setDeltaY = (state: KeyBindingsState, deltaY: number): KeyBindingsS
   }
   const minLineY = Math.floor(deltaY / itemHeight)
   const maxLineY = minLineY + Math.round(tableHeight / itemHeight)
+  const visibleItems = getVisibleKeyBindings(items, minLineY, maxLineY, selectedIndex, editingWhenExpression)
   return {
     ...state,
     deltaY,
     minLineY,
     maxLineY,
+    visibleItems,
   }
 }
