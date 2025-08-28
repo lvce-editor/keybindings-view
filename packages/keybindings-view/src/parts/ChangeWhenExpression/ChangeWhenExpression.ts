79c6f280211aaa5859e506508cd91e26c31451e4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-28 11:51:55 +0200

    feature: update focus key when changing when expression (#399)

--- packages/keybindings-view/src/parts/ChangeWhenExpression/ChangeWhenExpression.ts
+++ packages/keybindings-view/src/parts/ChangeWhenExpression/ChangeWhenExpression.ts
@@ -1,8 +1,10 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const changeWhenExpression = (state: KeyBindingsState): KeyBindingsState => {
   return {
     ...state,
     editingWhenExpression: true,
+    focus: WhenExpression.FocusKeyBindingsWhenExpression,
   }
 }
