7f61701e988af00f847f1ff479baad02f186e171
Le Vivilet <levivlet@pm.me>
2025-08-28 09:34:42 +0200

    feature: update diff modules

--- packages/keybindings-view/src/parts/DiffModules/DiffModules.ts
+++ packages/keybindings-view/src/parts/DiffModules/DiffModules.ts
@@ -4,8 +4,16 @@ import * as DiffFocusContext from '../DiffFocusContext/DiffFocusContext.ts'
 import * as DiffKeyBindings from '../DiffKeyBindings/DiffKeyBindings.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
+import * as DiffWhenExpressionText from '../DiffWhenExpressionText/DiffWhenExpressionText.ts'
 
-export const modules = [DiffKeyBindings.isEqual, DiffColumnWidths.isEqual, DiffValue.isEqual, DiffFocus.isEqual, DiffFocusContext.isEqual]
+export const modules = [
+  DiffKeyBindings.isEqual,
+  DiffColumnWidths.isEqual,
+  DiffValue.isEqual,
+  DiffFocus.isEqual,
+  DiffFocusContext.isEqual,
+  DiffWhenExpressionText.isEqual,
+]
 
 export const numbers = [
   DiffType.RenderKeyBindings,
@@ -13,4 +21,5 @@ export const numbers = [
   DiffType.RenderValue,
   DiffType.RenderFocus,
   DiffType.RenderFocusContext,
+  DiffType.RenderWhenExpressionText,
 ]
