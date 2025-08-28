ee08ad32807f3ec609eb8ea9309e1f0016c653c8
Le Vivilet <levivlet@pm.me>
2025-08-28 18:05:46 +0200

    feature: add addKeyBinding test

---
+++ packages/keybindings-view/test/AddKeyBinding.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@jest/globals'
+import type { KeyBindingsState } from '../src/parts/KeyBindingsState/KeyBindingsState.ts'
+import * as AddKeyBinding from '../src/parts/AddKeyBinding/AddKeyBinding.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+
+test('addKeyBinding - returns same state (no-op)', () => {
+  const state: KeyBindingsState = {
+    ...createDefaultState(),
+    value: 'x',
+  }
+
+  const result: KeyBindingsState = AddKeyBinding.addKeyBinding(state)
+
+  expect(result).toBe(state)
+})
