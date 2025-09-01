31db25856ac88f446a70a1cdfe8cd1ee50bd4dc2
Le Vivilet <levivlet@pm.me>
2025-09-01 22:11:16 +0200

    fix

--- packages/keybindings-view/src/parts/RendererWorker/RendererWorker.ts
+++ packages/keybindings-view/src/parts/RendererWorker/RendererWorker.ts
@@ -2,8 +2,8 @@ import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const { invoke, set, writeClipBoardText, setFocus, showContextMenu, openWidget } = RendererWorker
 
-export const registerMockRpc = (mockRpc: unknown): void => {
+export const registerMockRpc = (mockRpc: unknown): any => {
   // forward to underlying implementation while avoiding exporting private types
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ;(RendererWorker as any).registerMockRpc(mockRpc)
+  return (RendererWorker as any).registerMockRpc(mockRpc)
 }
