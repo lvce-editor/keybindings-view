2e6ce3f4565d158ff18f0382bab5f87ce20ea3b1
Le Vivilet <levivilet@pm.me>
2025-11-12 23:45:16 +0100

    fix: invalid css

--- packages/keybindings-view/src/parts/GetCss/GetCss.ts
+++ packages/keybindings-view/src/parts/GetCss/GetCss.ts
@@ -1,9 +1,9 @@
 export const getCss = (columnWidth1: number, columnWidth2: number, columnWidth3: number): string => {
   const rules = [
     `.KeyBindings {
-  --TableColumnOneWidth: var(--${columnWidth1}px);
-  --TableColumnTwoWidth: var(--${columnWidth2}px);
-  --TableColumnThreeWidth: var(--${columnWidth3}px);
+  --TableColumnOneWidth: --${columnWidth1}px;
+  --TableColumnTwoWidth: --${columnWidth2}px;
+  --TableColumnThreeWidth: --${columnWidth3}px;
 }`,
   ]
   const css = rules.join('\n')
