ca2e47cba38562aef3c499235170adeef18a47f9
Le Vivilet <levivilet@pm.me>
2025-11-12 23:47:00 +0100

    fix

--- packages/keybindings-view/src/parts/GetCss/GetCss.ts
+++ packages/keybindings-view/src/parts/GetCss/GetCss.ts
@@ -1,8 +1,8 @@
 export const getCss = (columnWidth0: number, columnWidth1: number, columnWidth2: number, columnWidth3: number): string => {
   const rules = [
     `.KeyBindings {
-  --TableColumnOneWidth: --${columnWidth0}px;
-  --TableColumnZeroWidth: --${columnWidth1}px;
+  --TableColumnZeroWidth: --${columnWidth0}px;
+  --TableColumnOneWidth: --${columnWidth1}px;
   --TableColumnTwoWidth: --${columnWidth2}px;
   --TableColumnThreeWidth: --${columnWidth3}px;
 }`,
