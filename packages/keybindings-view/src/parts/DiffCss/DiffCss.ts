625a5d9471e325397a271730759d2dd3e9fb4b52
Le Vivilet <levivilet@pm.me>
2025-11-12 23:42:23 +0100

    feature: update diff

--- packages/keybindings-view/src/parts/DiffCss/DiffCss.ts
+++ packages/keybindings-view/src/parts/DiffCss/DiffCss.ts
@@ -2,6 +2,7 @@ import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 
 export const isEqual = (oldState: KeyBindingsState, newState: KeyBindingsState): boolean => {
   return (
+    oldState.columnWidth0 === newState.columnWidth0 &&
     oldState.columnWidth1 === newState.columnWidth1 &&
     oldState.columnWidth2 === newState.columnWidth2 &&
     oldState.columnWidth3 === newState.columnWidth3
