aa2469a911f50dcced0ac95ce41d3dae4f3f13fb
Le Vivilet <levivilet@pm.me>
2025-11-12 23:57:25 +0100

    feature: render resizer left as css variables

--- packages/keybindings-view/src/parts/GetCss/GetCss.ts
+++ packages/keybindings-view/src/parts/GetCss/GetCss.ts
@@ -1,10 +1,19 @@
-export const getCss = (columnWidth0: number, columnWidth1: number, columnWidth2: number, columnWidth3: number): string => {
+export const getCss = (
+  columnWidth0: number,
+  columnWidth1: number,
+  columnWidth2: number,
+  columnWidth3: number,
+  resizerOneLeft: number,
+  resizerTwoLeft: number,
+): string => {
   const rules = [
     `.KeyBindings {
   --TableColumnZeroWidth: ${columnWidth0}px;
   --TableColumnOneWidth: ${columnWidth1}px;
   --TableColumnTwoWidth: ${columnWidth2}px;
   --TableColumnThreeWidth: ${columnWidth3}px;
+  --ResizerOneLeft: ${resizerOneLeft}px;
+  --ResizerTwoLeft: ${resizerTwoLeft}px;
 }`,
   ]
   const css = rules.join('\n')
