2a6d6db5b04df2205a7de26631afccee5a4207a3
Le Vivilet <levivilet@pm.me>
2025-11-12 23:58:46 +0100

    feature: render resizer left as css variables (#507)

--- packages/keybindings-view/src/parts/RenderCss/RenderCss.ts
+++ packages/keybindings-view/src/parts/RenderCss/RenderCss.ts
@@ -3,7 +3,7 @@ import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import { getCss } from '../GetCss/GetCss.ts'
 
 export const renderCss = (oldState: KeyBindingsState, newState: KeyBindingsState): readonly any[] => {
-  const { uid, columnWidth0, columnWidth1, columnWidth2, columnWidth3 } = newState
-  const css = getCss(columnWidth0, columnWidth1, columnWidth2, columnWidth3)
+  const { uid, columnWidth0, columnWidth1, columnWidth2, columnWidth3, resizerOneLeft, resizerTwoLeft } = newState
+  const css = getCss(columnWidth0, columnWidth1, columnWidth2, columnWidth3, resizerOneLeft, resizerTwoLeft)
   return [ViewletCommand.SetCss, uid, css]
 }
