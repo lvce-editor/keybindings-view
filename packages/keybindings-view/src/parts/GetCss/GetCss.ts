914f2961c7c392e81e1c538e0d8016f78c85d01e
Le Vivilet <levivilet@pm.me>
2025-11-12 23:32:47 +0100

    feature: update css renderer (#497)

--- packages/keybindings-view/src/parts/GetCss/GetCss.ts
+++ packages/keybindings-view/src/parts/GetCss/GetCss.ts
@@ -1,7 +1,9 @@
-export const getCss = (): string => {
-  // TODO render width of colgroups
+export const getCss = (columnWidth1: number, columnWidth2: number, columnWidth3: number): string => {
   const rules = [
-    `.Explorer {
+    `.KeyBindings {
+  --TableColumnOneWidth: var(--${columnWidth1}px);
+  --TableColumnTwoWidth: var(--${columnWidth2}px);
+  --TableColumnThreeWidth: var(--${columnWidth3}px);
 }`,
   ]
   const css = rules.join('\n')
