ce369bf8a1dbe0b69aa090aaac4e9b6aeee63bf2
Le Vivilet <levivilet@pm.me>
2025-11-13 00:00:44 +0100

    feature: update resizer position on resize

--- packages/keybindings-view/src/parts/Resize/Resize.ts
+++ packages/keybindings-view/src/parts/Resize/Resize.ts
@@ -2,17 +2,21 @@ import type { Dimensions } from '../Dimensions/Dimensions.ts'
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 
 export const resize = (state: KeyBindingsState, dimensions: Dimensions): KeyBindingsState => {
-  const { contentPadding } = state
+  const { contentPadding, columnWidth0 } = state
   const { width } = dimensions
   const contentWidth = width - contentPadding
   const columnWidth1 = contentWidth / 3
   const columnWidth2 = contentWidth / 3
   const columnWidth3 = contentWidth / 3
+  const resizerOneLeft = columnWidth0 + columnWidth1
+  const resizerTwoLeft = columnWidth0 + columnWidth1 + columnWidth2
   return {
     ...state,
     ...dimensions,
     columnWidth1,
     columnWidth2,
     columnWidth3,
+    resizerOneLeft,
+    resizerTwoLeft,
   }
 }
