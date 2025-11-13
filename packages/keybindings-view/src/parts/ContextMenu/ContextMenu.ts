0cfafa1ec9c4397c1f3d7ebddf20b7a789414d41
Le Vivilet <levivilet@pm.me>
2025-11-13 12:47:28 +0100

    feature: add showContextMenu2 (#525)

--- packages/keybindings-view/src/parts/ContextMenu/ContextMenu.ts
+++ packages/keybindings-view/src/parts/ContextMenu/ContextMenu.ts
@@ -3,3 +3,7 @@ import { RendererWorker } from '@lvce-editor/rpc-registry'
 export const show = async (x: number, y: number, id: number, ...args: readonly any[]): Promise<void> => {
   await RendererWorker.showContextMenu(x, y, id, ...args)
 }
+
+export const show2 = async (uid: number, menuId: any, x: number, y: number, args: any): Promise<void> => {
+  await RendererWorker.showContextMenu2(uid, menuId, x, y, args)
+}
