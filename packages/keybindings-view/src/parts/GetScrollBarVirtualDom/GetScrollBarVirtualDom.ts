6897e45bd22b474e0425f8dde436be4e1ce571de
Le Vivilet <levivilet@pm.me>
2025-11-13 12:32:23 +0100

    tsc

--- packages/keybindings-view/src/parts/GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts
@@ -1,7 +1,6 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as Px from '../Px/Px.ts'
 
 const scrollbarNode: VirtualDomNode = {
   type: VirtualDomElements.Div,
