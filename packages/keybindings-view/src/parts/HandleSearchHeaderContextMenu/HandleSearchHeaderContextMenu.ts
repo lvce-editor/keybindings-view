ae567ef6f2656032feff4dae82f6ab66efacaa01
Le Vivilet <levivilet@pm.me>
2025-11-13 12:41:59 +0100

    feature: add search context menu function (#520)

---
+++ packages/keybindings-view/src/parts/HandleSearchHeaderContextMenu/HandleSearchHeaderContextMenu.ts
@@ -0,0 +1,5 @@
+import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+
+export const handleSearchHeaderContextMenu = async (state: KeyBindingsState): Promise<KeyBindingsState> => {
+  return state
+}
