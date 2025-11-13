ca9006325c0a90b4a22b3be58d4f892a4c897201
Le Vivilet <levivilet@pm.me>
2025-11-13 12:48:33 +0100

    fix

--- packages/e2e/src/keybindings.open-context-menu.ts
+++ packages/e2e/src/keybindings.open-context-menu.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'keybindings.open-context-menu'
 
+export const skip = 1
+
 export const test: Test = async ({ Locator, expect, KeyBindingsEditor }) => {
   // arrange
   await KeyBindingsEditor.open()
