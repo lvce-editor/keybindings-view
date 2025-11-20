3f655024b12bb7f1fb8b68b77df9f6158584129d
Le Vivilet <levivilet@pm.me>
2025-11-20 21:28:33 +0100

    feature: make restoreState typesafe (#560)

--- packages/keybindings-view/src/parts/RestoreState/RestoreState.ts
+++ packages/keybindings-view/src/parts/RestoreState/RestoreState.ts
@@ -1,17 +1,41 @@
 import type { RestoredState } from '../RestoredState/RestoredState.ts'
 import * as GetSavedValue from '../GetSavedValue/GetSavedValue.ts'
+import { hasProperty } from '../HasProperty/HasProperty.ts'
+
+const getSavedIsRecordingKeys = (savedState: unknown): boolean => {
+  if (hasProperty(savedState, 'isRecordingKeys') && typeof savedState.isRecordingKeys === 'boolean') {
+    return savedState.isRecordingKeys
+  }
+  return false
+}
+
+const getSavedIsSortingByPrecedence = (savedState: unknown): boolean => {
+  if (hasProperty(savedState, 'isSortingByPrecedence') && typeof savedState.isSortingByPrecedence === 'boolean') {
+    return savedState.isSortingByPrecedence
+  }
+  return false
+}
+
+const getSavedFocus = (savedState: unknown): number => {
+  if (hasProperty(savedState, 'focus') && typeof savedState.focus === 'number') {
+    return savedState.focus
+  }
+  return 0
+}
+
+const getSavedSelectedIndex = (savedState: unknown): number => {
+  if (hasProperty(savedState, 'selectedIndex') && typeof savedState.selectedIndex === 'number') {
+    return savedState.selectedIndex
+  }
+  return -1
+}
 
 export const restoreState = (savedState: unknown): RestoredState => {
-  // TODO make it type safe
   const savedValue = GetSavedValue.getSavedValue(savedState)
-  // @ts-ignore
-  const isRecordingKeys = savedState?.['isRecordingKeys']
-  // @ts-ignore
-  const isSortingByPrecedence = savedState?.['isSortingByPrecedence']
-  // @ts-ignore
-  const focus = savedState?.['focus']
-  // @ts-ignore
-  const selectedIndex = savedState?.['selectedIndex']
+  const isRecordingKeys = getSavedIsRecordingKeys(savedState)
+  const isSortingByPrecedence = getSavedIsSortingByPrecedence(savedState)
+  const focus = getSavedFocus(savedState)
+  const selectedIndex = getSavedSelectedIndex(savedState)
   return {
     savedValue,
     isRecordingKeys,
