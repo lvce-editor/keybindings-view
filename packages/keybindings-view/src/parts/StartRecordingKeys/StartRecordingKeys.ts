331639007d03c7f5d10bd56d0177f6469baa36a8
Le Vivilet <levivilet@pm.me>
2025-11-20 17:30:41 +0100

    feature: add separate function for recording keys width (#548)

--- packages/keybindings-view/src/parts/StartRecordingKeys/StartRecordingKeys.ts
+++ packages/keybindings-view/src/parts/StartRecordingKeys/StartRecordingKeys.ts
@@ -1,14 +1,8 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
-import * as KeyBindingStrings from '../KeyBindingStrings/KeyBindingStrings.ts'
-import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
+import { getRecordingKeysLabelWidth } from '../GetRecordingKeysLabelWidth/GetRecordingKeysLabelWidth.ts'
 
 export const startRecordingKeys = async (state: KeyBindingsState): Promise<KeyBindingsState> => {
-  const label = KeyBindingStrings.recordingKeys()
-  const fontFamily = 'system-ui, Ubuntu'
-  const fontSize = 13
-  const fontWeight = 400
-  const letterSpacing = 0.5
-  const recordingKeysLabelWidth = await MeasureTextWidth.measureTextWidth(label, fontWeight, fontSize, fontFamily, letterSpacing)
+  const recordingKeysLabelWidth = await getRecordingKeysLabelWidth()
   return {
     ...state,
     isRecordingKeys: true,
