dfe30eb2a01013d8dc47ec555eca3bff6a7750d5
Le Vivilet <levivilet@pm.me>
2025-11-20 17:14:17 +0100

    feature: measure width when toggling recordKeys

--- packages/keybindings-view/src/parts/StartRecordingKeys/StartRecordingKeys.ts
+++ packages/keybindings-view/src/parts/StartRecordingKeys/StartRecordingKeys.ts
@@ -1,8 +1,17 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as KeyBindingStrings from '../KeyBindingStrings/KeyBindingStrings.ts'
+import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
 
-export const startRecordingKeys = (state: KeyBindingsState): KeyBindingsState => {
+export const startRecordingKeys = async (state: KeyBindingsState): Promise<KeyBindingsState> => {
+  const label = KeyBindingStrings.recordingKeys()
+  const fontFamily = ''
+  const fontSize = 14
+  const fontWeight = 500
+  const letterSpacing = 0.5
+  const recordingKeysLabelWidth = await MeasureTextWidth.measureTextWidth(label, fontWeight, fontSize, fontFamily, letterSpacing)
   return {
     ...state,
     isRecordingKeys: true,
+    recordingKeysLabelWidth,
   }
 }
--- packages/keybindings-view/test/StartRecordingKeys.test.ts
+++ packages/keybindings-view/test/StartRecordingKeys.test.ts
@@ -3,9 +3,9 @@ import * as Create from '../src/parts/Create/Create.ts'
 import * as KeyBindingsStates from '../src/parts/KeyBindingsStates/KeyBindingsStates.ts'
 import * as StartRecordingKeys from '../src/parts/StartRecordingKeys/StartRecordingKeys.ts'
 
-test('startRecordingKeys', () => {
+test('startRecordingKeys', async () => {
   Create.create(1, '', 0, 0, 0, 0, 0)
   const { oldState } = KeyBindingsStates.get(1)
-  const newState = StartRecordingKeys.startRecordingKeys(oldState)
+  const newState = await StartRecordingKeys.startRecordingKeys(oldState)
   expect(newState.isRecordingKeys).toBe(true)
 })
