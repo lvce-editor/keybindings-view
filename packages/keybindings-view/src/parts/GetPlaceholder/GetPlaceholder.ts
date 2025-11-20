effc5ee109f8fb4e7839f2b734ad53f278bbb8ea
Le Vivilet <levivilet@pm.me>
2025-11-20 21:39:57 +0100

    feature: update placeholder string

--- packages/keybindings-view/src/parts/GetPlaceholder/GetPlaceholder.ts
+++ packages/keybindings-view/src/parts/GetPlaceholder/GetPlaceholder.ts
@@ -2,7 +2,7 @@ import * as KeyBindingStrings from '../KeyBindingStrings/KeyBindingStrings.ts'
 
 export const getPlaceholder = (isRecordingKeys: boolean): string => {
   if (isRecordingKeys) {
-    return '' // TODO
+    return KeyBindingStrings.recordingKeysPressEscapeToExit()
   }
   return KeyBindingStrings.typeToSearchKeyBindings()
 }
