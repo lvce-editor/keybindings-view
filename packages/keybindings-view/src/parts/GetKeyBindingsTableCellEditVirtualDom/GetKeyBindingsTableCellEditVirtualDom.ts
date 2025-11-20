87cdb5d500cf74590b83da7d5686f06b29a36e58
Le Vivilet <levivilet@pm.me>
2025-11-21 00:19:13 +0100

    order

--- packages/keybindings-view/src/parts/GetKeyBindingsTableCellEditVirtualDom/GetKeyBindingsTableCellEditVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetKeyBindingsTableCellEditVirtualDom/GetKeyBindingsTableCellEditVirtualDom.ts
@@ -3,9 +3,11 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetIconVirtualDom from '../GetIconVirtualDom/GetIconVirtualDom.ts'
 import * as Icon from '../Icon/Icon.ts'
+import * as KeyBindingStrings from '../KeyBindingStrings/KeyBindingStrings.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
 export const getKeyBindingsTableEditCellDom = (): readonly VirtualDomNode[] => {
+  const title = KeyBindingStrings.edit()
   return [
     {
       type: VirtualDomElements.Td,
@@ -16,6 +18,7 @@ export const getKeyBindingsTableEditCellDom = (): readonly VirtualDomNode[] => {
       type: VirtualDomElements.Button,
       className: MergeClassNames.mergeClassNames(ClassNames.IconButton, ClassNames.KeyBindingsEditButton),
       tabIndex: -1,
+      title,
       childCount: 1,
     },
     GetIconVirtualDom.getIconVirtualDom(Icon.Edit),
