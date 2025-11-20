d8fd05a6c453031aa435abcfa5ecfafa51ac329a
Le Vivilet <levivilet@pm.me>
2025-11-20 17:12:56 +0100

    feature: make toggleRecordingKeys async

--- packages/keybindings-view/src/parts/ToggleRecordingKeys/ToggleRecordingKeys.ts
+++ packages/keybindings-view/src/parts/ToggleRecordingKeys/ToggleRecordingKeys.ts
@@ -2,7 +2,7 @@ import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as StartRecordingKeys from '../StartRecordingKeys/StartRecordingKeys.ts'
 import * as StopRecordingKeys from '../StopRecordingKeys/StopRecordingKeys.ts'
 
-export const toggleRecordingKeys = (state: KeyBindingsState): KeyBindingsState => {
+export const toggleRecordingKeys = async (state: KeyBindingsState): Promise<KeyBindingsState> => {
   const { isRecordingKeys } = state
   if (isRecordingKeys) {
     return StopRecordingKeys.stopRecordingKeys(state)
