7522121f5deb71399e317ce3eaa83b3b3374204c
Le Vivilet <levivilet@pm.me>
2025-11-20 21:43:13 +0100

    feature: update placeholder when changing isRecordingKeys (#565)

--- packages/keybindings-view/src/parts/StopRecordingKeys/StopRecordingKeys.ts
+++ packages/keybindings-view/src/parts/StopRecordingKeys/StopRecordingKeys.ts
@@ -1,8 +1,11 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import { getPlaceholder } from '../GetPlaceholder/GetPlaceholder.ts'
 
 export const stopRecordingKeys = (state: KeyBindingsState): KeyBindingsState => {
+  const placeholder = getPlaceholder(false)
   return {
     ...state,
     isRecordingKeys: false,
+    placeholder,
   }
 }
