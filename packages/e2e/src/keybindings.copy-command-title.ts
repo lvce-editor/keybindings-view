e8a2ace60c5a20432ecaa7911dda4f18fd2c8f4a
Le Vivilet <levivilet@pm.me>
2025-11-20 13:30:41 +0100

    feature: enable copyCommandTitle e2e test

--- packages/e2e/src/keybindings.copy-command-title.ts
+++ packages/e2e/src/keybindings.copy-command-title.ts
@@ -2,16 +2,16 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'keybindings.copy-command-title'
 
-export const skip = 1
-
-export const test: Test = async ({ KeyBindingsEditor }) => {
+export const test: Test = async ({ KeyBindingsEditor, ClipBoard }) => {
   // arrange
+  await ClipBoard.enableMemoryClipBoard()
   await KeyBindingsEditor.open()
   await KeyBindingsEditor.handleInput('About.focus')
+  await KeyBindingsEditor.focusFirst()
 
   // act
   await KeyBindingsEditor.copyCommandTitle()
 
   // assert
-  // await expect(input).toHaveText('')
+  await ClipBoard.shouldHaveText('About.focusNext')
 }
