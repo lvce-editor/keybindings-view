edc110d5b7632c34b66f5b3d0cc36a53ba92b5c2
Le Vivilet <levivilet@pm.me>
2025-11-20 22:11:37 +0100

    feature: add focusindex e2e test (#575)

---
+++ packages/e2e/src/keybindings.focus-index.ts
@@ -0,0 +1,16 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'keybindings.focus-last'
+
+export const test: Test = async ({ KeyBindingsEditor, Locator, expect }) => {
+  // arrange
+  await KeyBindingsEditor.open()
+  await KeyBindingsEditor.handleInput('About.focus')
+
+  // act
+  await KeyBindingsEditor.focusIndex(1)
+
+  // assert
+  const row = Locator('.TableRow[aria-rowindex="3"]')
+  await expect(row).toHaveClass('TableRowSelected')
+}
