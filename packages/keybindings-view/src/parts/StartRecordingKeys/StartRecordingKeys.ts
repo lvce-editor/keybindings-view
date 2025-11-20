e3a8ea0d48c21572e01587b85bc72b15d4a72e62
Le Vivilet <levivilet@pm.me>
2025-11-20 17:24:42 +0100

    feature: measure width when toggling recordKeys (#542)
    * feature: measure width when toggling recordKeys
    
    * width
    
    * memory

--- packages/keybindings-view/src/parts/StartRecordingKeys/StartRecordingKeys.ts
+++ packages/keybindings-view/src/parts/StartRecordingKeys/StartRecordingKeys.ts
@@ -1,8 +1,17 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as KeyBindingStrings from '../KeyBindingStrings/KeyBindingStrings.ts'
+import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
 
-export const startRecordingKeys = (state: KeyBindingsState): KeyBindingsState => {
+export const startRecordingKeys = async (state: KeyBindingsState): Promise<KeyBindingsState> => {
+  const label = KeyBindingStrings.recordingKeys()
+  const fontFamily = 'system-ui, Ubuntu'
+  const fontSize = 13
+  const fontWeight = 400
+  const letterSpacing = 0.5
+  const recordingKeysLabelWidth = await MeasureTextWidth.measureTextWidth(label, fontWeight, fontSize, fontFamily, letterSpacing)
   return {
     ...state,
     isRecordingKeys: true,
+    recordingKeysLabelWidth,
   }
 }
