1d469378209b7af183809b5be1f585116f3f24ee
Le Vivilet <levivilet@pm.me>
2025-11-20 21:39:11 +0100

    feature: update placeholder when changing isRecordingKeys

--- packages/keybindings-view/src/parts/StartRecordingKeys/StartRecordingKeys.ts
+++ packages/keybindings-view/src/parts/StartRecordingKeys/StartRecordingKeys.ts
@@ -1,11 +1,14 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import { getPlaceholder } from '../GetPlaceholder/GetPlaceholder.ts'
 import { getRecordingKeysLabelWidth } from '../GetRecordingKeysLabelWidth/GetRecordingKeysLabelWidth.ts'
 
 export const startRecordingKeys = async (state: KeyBindingsState): Promise<KeyBindingsState> => {
   const recordingKeysLabelWidth = await getRecordingKeysLabelWidth(true)
+  const placeholder = getPlaceholder(true)
   return {
     ...state,
     isRecordingKeys: true,
+    placeholder,
     recordingKeysLabelWidth,
   }
 }
