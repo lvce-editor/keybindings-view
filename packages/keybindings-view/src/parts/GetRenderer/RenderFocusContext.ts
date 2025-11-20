2929d0791873c25c64f4ac693a203b997fbd1ad2
Le Vivilet <levivilet@pm.me>
2025-11-20 22:03:39 +0100

    feature: use destructuring (#571)

--- packages/keybindings-view/src/parts/GetRenderer/RenderFocusContext.ts
+++ packages/keybindings-view/src/parts/GetRenderer/RenderFocusContext.ts
@@ -2,5 +2,6 @@ import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const renderFocusContext = (oldState: KeyBindingsState, newState: KeyBindingsState): readonly any[] => {
-  return ['Viewlet.focusSelector', newState.uid, InputName.KeyBindingsFilter]
+  const { uid } = newState
+  return ['Viewlet.focusSelector', uid, InputName.KeyBindingsFilter]
 }
