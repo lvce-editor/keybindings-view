ac3fcca431fbbdca014634c70cbbd5df0feeb584
Le Vivilet <levivilet@pm.me>
2025-11-20 22:12:25 +0100

    feature: add handleClickIndex

--- packages/keybindings-view/src/parts/HandleClick/HandleClick.ts
+++ packages/keybindings-view/src/parts/HandleClick/HandleClick.ts
@@ -1,19 +1,11 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
-import * as FocusKey from '../FocusKey/FocusKey.ts'
 import * as GetIndex from '../GetIndex/GetIndex.ts'
-import * as ShowDefineKeyBinding from '../ShowDefineKeyBinding/ShowDefineKeyBinding.ts'
+import * as HandleClickIndex from '../HandleClickIndex/HandleClickIndex.ts'
 
 export const handleClick = async (state: KeyBindingsState, eventX: number, eventY: number): Promise<KeyBindingsState> => {
   const { padding, editIconSize, x } = state
-  const selectedIndex = GetIndex.getIndex(state, eventX, eventY)
+  const index = GetIndex.getIndex(state, eventX, eventY)
   const relativeX = eventX - x
-  if (relativeX > padding && relativeX < padding + editIconSize) {
-    await ShowDefineKeyBinding.showDefineKeyBinding()
-  }
-  return {
-    ...state,
-    focusedIndex: selectedIndex,
-    selectedIndex,
-    focus: FocusKey.Table,
-  }
+  const showDefine = relativeX > padding && relativeX < padding + editIconSize
+  return HandleClickIndex.handleClickIndex(state, index, showDefine)
 }
