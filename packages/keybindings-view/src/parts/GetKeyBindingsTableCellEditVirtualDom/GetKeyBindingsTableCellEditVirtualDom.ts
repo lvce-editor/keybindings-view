ae5e5648ace4d4a9fbf2437b821b569abcf7c4ea
Le Vivilet <levivilet@pm.me>
2025-11-21 00:41:25 +0100

    feature: add title to button (#581)

--- packages/keybindings-view/src/parts/GetKeyBindingsTableCellEditVirtualDom/GetKeyBindingsTableCellEditVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetKeyBindingsTableCellEditVirtualDom/GetKeyBindingsTableCellEditVirtualDom.ts
@@ -3,6 +3,7 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetIconVirtualDom from '../GetIconVirtualDom/GetIconVirtualDom.ts'
 import * as Icon from '../Icon/Icon.ts'
+import * as KeyBindingStrings from '../KeyBindingStrings/KeyBindingStrings.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
 const cell: VirtualDomNode = {
@@ -16,11 +17,14 @@ const icon: VirtualDomNode = GetIconVirtualDom.getIconVirtualDom(Icon.Edit)
 const className = MergeClassNames.mergeClassNames(ClassNames.IconButton, ClassNames.KeyBindingsEditButton)
 
 export const getKeyBindingsTableEditCellDom = (): readonly VirtualDomNode[] => {
+  const title = KeyBindingStrings.edit()
   return [
     cell,
     {
       type: VirtualDomElements.Button,
       className,
+      tabIndex: -1,
+      title,
       childCount: 1,
     },
     icon,
