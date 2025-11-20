39cfca474999edf8a3bcd0d7e5c67613572d7d1e
Le Vivilet <levivilet@pm.me>
2025-11-21 00:47:45 +0100

    feature: use viewlet command (#594)

--- packages/keybindings-view/src/parts/GetRenderer/RenderFocusContext.ts
+++ packages/keybindings-view/src/parts/GetRenderer/RenderFocusContext.ts
@@ -1,7 +1,8 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const renderFocusContext = (oldState: KeyBindingsState, newState: KeyBindingsState): readonly any[] => {
   const { uid } = newState
-  return ['Viewlet.focusSelector', uid, InputName.KeyBindingsFilter]
+  return [ViewletCommand.FocusSelector, uid, InputName.KeyBindingsFilter]
 }
