6a0cc6e3f559e842efb7cc064abef42996d0ca02
Le Vivilet <levivilet@pm.me>
2025-11-21 17:51:12 +0100

    feature: rerender when recording state changes (#612)

--- packages/keybindings-view/src/parts/DiffKeyBindings/DiffKeyBindings.ts
+++ packages/keybindings-view/src/parts/DiffKeyBindings/DiffKeyBindings.ts
@@ -2,13 +2,14 @@ import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 
 export const isEqual = (oldState: KeyBindingsState, newState: KeyBindingsState): boolean => {
   return (
-    oldState.items === newState.items &&
-    oldState.minLineY === newState.minLineY &&
-    oldState.maxLineY === newState.maxLineY &&
-    oldState.selectedIndex === newState.selectedIndex &&
+    oldState.editingWhenExpression === newState.editingWhenExpression &&
     oldState.focusedIndex === newState.focusedIndex &&
     oldState.isRecordingKeys === newState.isRecordingKeys &&
+    oldState.isRecordingKeys === newState.isRecordingKeys &&
     oldState.isSortingByPrecedence === newState.isSortingByPrecedence &&
-    oldState.editingWhenExpression === newState.editingWhenExpression
+    oldState.items === newState.items &&
+    oldState.maxLineY === newState.maxLineY &&
+    oldState.minLineY === newState.minLineY &&
+    oldState.selectedIndex === newState.selectedIndex
   )
 }
