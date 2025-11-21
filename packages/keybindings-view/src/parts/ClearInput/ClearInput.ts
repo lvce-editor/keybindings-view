24e122dc9267229b47ec4624770a50d479c336df
Le Vivilet <levivilet@pm.me>
2025-11-21 14:09:42 +0100

    feature: don't filter items when clearing search value and it is already empty

--- packages/keybindings-view/src/parts/ClearInput/ClearInput.ts
+++ packages/keybindings-view/src/parts/ClearInput/ClearInput.ts
@@ -3,6 +3,10 @@ import * as HandleInput from '../HandleInput/HandleInput.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
 export const clearInput = (state: KeyBindingsState): KeyBindingsState => {
+  const { value } = state
+  if (value === '') {
+    return state
+  }
   const newState = HandleInput.handleInput(state, '')
   return {
     ...newState,
