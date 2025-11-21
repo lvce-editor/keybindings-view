ab77262541c4f5a1b1922f6044e0504156d65c76
Le Vivilet <levivilet@pm.me>
2025-11-21 14:15:46 +0100

    feature: add fast case for diff

--- packages/keybindings-view/src/parts/Diff/Diff.ts
+++ packages/keybindings-view/src/parts/Diff/Diff.ts
@@ -2,6 +2,9 @@ import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import * as DiffModules from '../DiffModules/DiffModules.ts'
 
 export const diff = (oldState: KeyBindingsState, newState: KeyBindingsState): readonly number[] => {
+  if (oldState === newState) {
+    return []
+  }
   const diffResult: number[] = []
   for (let i = 0; i < DiffModules.modules.length; i++) {
     const fn = DiffModules.modules[i]
