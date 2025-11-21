cdea05c21503d63cbdb6316c9a77a717813df6ce
Le Vivilet <levivilet@pm.me>
2025-11-21 18:00:33 +0100

    feature: set resizer id on click (#617)

--- packages/keybindings-view/src/parts/HandleResizerClick/HandleResizerClick.ts
+++ packages/keybindings-view/src/parts/HandleResizerClick/HandleResizerClick.ts
@@ -1,8 +1,20 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 
+const getResizerId = (name: string): number => {
+  switch (name) {
+    case 'ResizerOne':
+      return 1
+    case 'ResizerTwo':
+      return 2
+    default:
+      return 0
+  }
+}
+
 export const handleResizerClick = (state: KeyBindingsState, name: string, x: number): KeyBindingsState => {
+  const resizerDownId = getResizerId(name)
   return {
     ...state,
-    resizerDownId: 1,
+    resizerDownId,
   }
 }
