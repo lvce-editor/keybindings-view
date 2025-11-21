369409a3fb7c1a83365d0792e66dbcb02500f1fb
Le Vivilet <levivilet@pm.me>
2025-11-21 17:54:45 +0100

    tsc

--- packages/keybindings-view/src/parts/HandleResizerClick/HandleResizerClick.ts
+++ packages/keybindings-view/src/parts/HandleResizerClick/HandleResizerClick.ts
@@ -1,7 +1,6 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 
 export const handleResizerClick = (state: KeyBindingsState, name: string, x: number): KeyBindingsState => {
-  console.log({ x })
   return {
     ...state,
     resizerDownId: 1,
