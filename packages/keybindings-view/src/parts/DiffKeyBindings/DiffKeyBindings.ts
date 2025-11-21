0a9ebbf7ab9021d8fed2128ff22e66b272868c83
Le Vivilet <levivilet@pm.me>
2025-11-21 17:26:24 +0100

    feature: don't rerender dom when column widths change

--- packages/keybindings-view/src/parts/DiffKeyBindings/DiffKeyBindings.ts
+++ packages/keybindings-view/src/parts/DiffKeyBindings/DiffKeyBindings.ts
@@ -7,9 +7,6 @@ export const isEqual = (oldState: KeyBindingsState, newState: KeyBindingsState):
     oldState.maxLineY === newState.maxLineY &&
     oldState.selectedIndex === newState.selectedIndex &&
     oldState.focusedIndex === newState.focusedIndex &&
-    oldState.columnWidth1 === newState.columnWidth1 &&
-    oldState.columnWidth2 === newState.columnWidth2 &&
-    oldState.columnWidth3 === newState.columnWidth3 &&
     oldState.isRecordingKeys === newState.isRecordingKeys &&
     oldState.isSortingByPrecedence === newState.isSortingByPrecedence &&
     oldState.editingWhenExpression === newState.editingWhenExpression
