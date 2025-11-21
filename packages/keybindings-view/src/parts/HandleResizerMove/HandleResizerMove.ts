c9704a2144a181d2d12710a29bc5cb7463ef7524
Le Vivilet <levivilet@pm.me>
2025-11-21 17:33:15 +0100

    feature: update resizer position on pointer move

--- packages/keybindings-view/src/parts/HandleResizerMove/HandleResizerMove.ts
+++ packages/keybindings-view/src/parts/HandleResizerMove/HandleResizerMove.ts
@@ -2,7 +2,7 @@ import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
 import { distributeColumns } from '../DistributeColumns/DistributeColumns.ts'
 
 export const handleResizerMove = (state: KeyBindingsState, eventX: number): KeyBindingsState => {
-  const { resizerDownId, contentPadding, width, columnWidth1, columnWidth2, x } = state
+  const { columnWidth0, resizerDownId, contentPadding, width, columnWidth1, columnWidth2, x } = state
   const { newColumnWidth1, newColumnWidth2, newColumnWidth3 } = distributeColumns(
     resizerDownId,
     contentPadding,
@@ -12,10 +12,14 @@ export const handleResizerMove = (state: KeyBindingsState, eventX: number): KeyB
     x,
     eventX,
   )
+  const resizerOneLeft = columnWidth0 + columnWidth1
+  const resizerTwoLeft = columnWidth0 + columnWidth1 + columnWidth2
   return {
     ...state,
     columnWidth1: newColumnWidth1,
     columnWidth2: newColumnWidth2,
     columnWidth3: newColumnWidth3,
+    resizerOneLeft,
+    resizerTwoLeft,
   }
 }
