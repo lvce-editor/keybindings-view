9767d5ab8d7b02ca70cb4145f8e49ebdd6f7e7b7
Le Vivilet <levivilet@pm.me>
2025-11-26 12:56:12 +0100

    feature: use new context menu handler (#623)

--- packages/keybindings-view/src/parts/HandleContextMenu/HandleContextMenu.ts
+++ packages/keybindings-view/src/parts/HandleContextMenu/HandleContextMenu.ts
@@ -3,11 +3,13 @@ import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 
 export const handleContextMenu = async (state: KeyBindingsState, button: number, eventX: number, eventY: number): Promise<KeyBindingsState> => {
-  const { y, searchHeaderHeight, tableHeaderHeight } = state
+  const { y, searchHeaderHeight, tableHeaderHeight, uid } = state
   if (eventY <= y + searchHeaderHeight + tableHeaderHeight) {
     return state
   }
   // TODO find the index of the matched item and if there is a matching entry, show context menu
-  await ContextMenu.show(eventX, eventY, MenuEntryId.KeyBindingsTable)
+  await ContextMenu.show2(uid, MenuEntryId.KeyBindingsTable, eventX, eventY, {
+    menuId: MenuEntryId.KeyBindingsTable,
+  })
   return state
 }
--- packages/keybindings-view/test/ContextMenu.test.ts
+++ packages/keybindings-view/test/ContextMenu.test.ts
@@ -9,6 +9,7 @@ test.skip('show - invokes context menu with correct items', async () => {
   const x = 100
   const y = 200
   const id = 1
+  // @ts-ignore
   await ContextMenu.show(x, y, id)
   expect(mockRpc.invocations).toEqual([['ContextMenu.show', x, y, id]])
 })
