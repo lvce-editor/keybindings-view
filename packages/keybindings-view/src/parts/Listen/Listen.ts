8f7e8ad587d2d559397e068f53fad30e50cf59f5
Le Vivilet <levivilet@pm.me>
2026-01-13 15:20:27 +0100

    feature: move initializeRendererWorker into separate file (#629)

--- packages/keybindings-view/src/parts/Listen/Listen.ts
+++ packages/keybindings-view/src/parts/Listen/Listen.ts
@@ -1,15 +1,11 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import { createTextMeasurementWorkerRpc } from '../CreateTextMeasurementWorkerRpc/CreateTextMeasurementWorkerRpc.ts'
+import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 import { registerCommands } from '../KeyBindingsStates/KeyBindingsStates.ts'
 import { setFactory } from '../TextMeasurementWorker/TextMeasurementWorker.ts'
 
 export const listen = async (): Promise<void> => {
   setFactory(createTextMeasurementWorkerRpc)
   registerCommands(CommandMap.commandMap)
-  const rpc = await WebWorkerRpcClient.create({
-    commandMap: CommandMap.commandMap,
-  })
-  RendererWorker.set(rpc)
+  await initializeRendererWorker()
 }
