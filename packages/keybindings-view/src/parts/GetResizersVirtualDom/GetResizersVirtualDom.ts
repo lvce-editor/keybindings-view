e0f89fe9adaaebf54a8b8ab35bcd8a2dfeaf445c
Le Vivilet <levivilet@pm.me>
2026-01-26 11:54:21 +0100

    feature: move resizers into separate div

--- packages/keybindings-view/src/parts/GetResizersVirtualDom/GetResizersVirtualDom.ts
+++ packages/keybindings-view/src/parts/GetResizersVirtualDom/GetResizersVirtualDom.ts
@@ -1,7 +1,16 @@
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getResizerVirtualDom } from '../GetResizerVirtualDom/GetResizerVirtualDom.ts'
 
 export const getResizersVirtualDom = (): readonly VirtualDomNode[] => {
-  return [...getResizerVirtualDom(ClassNames.ResizerOne), ...getResizerVirtualDom(ClassNames.ResizerTwo)]
+  return [
+    {
+      childCount: 4,
+      className: ClassNames.Resizers,
+      type: VirtualDomElements.Div,
+    },
+    ...getResizerVirtualDom(ClassNames.ResizerOne),
+    ...getResizerVirtualDom(ClassNames.ResizerTwo),
+  ]
 }
