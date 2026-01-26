cbd2a0875145b4f24919ecea4ddd622d4d072850
Le Vivilet <levivilet@pm.me>
2026-01-26 12:13:13 +0100

    fix

--- packages/keybindings-view/test/Resize.test.ts
+++ packages/keybindings-view/test/Resize.test.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@jest/globals'
 import type { Dimensions } from '../src/parts/Dimensions/Dimensions.ts'
 import type { KeyBindingsState } from '../src/parts/KeyBindingsState/KeyBindingsState.ts'
-import * as Resize from '../src/parts/Resize/Resize.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as Resize from '../src/parts/Resize/Resize.ts'
 
 test('resize - basic dimensions', () => {
   const state: KeyBindingsState = {
@@ -26,7 +26,12 @@ test('resize - basic dimensions', () => {
     ...dimensions,
     columnWidth1: 193.333_333_333_333_34,
     columnWidth2: 193.333_333_333_333_34,
-    columnWidth3: 193.333_333_333_333_34,
+    columnWidth3: 173.333_333_333_333_34,
+    maxVisibleItems: 17,
+    resizerOneLeft: 213.333_333_333_333_34,
+    resizerTwoLeft: 406.666_666_666_666_68,
+    scrollBarHeight: 0,
+    visibleItems: [],
   })
 })
 
@@ -50,9 +55,14 @@ test('resize - zero width', () => {
   expect(newState).toEqual({
     ...state,
     ...dimensions,
-    columnWidth1: -3.333_333_333_333_333_5,
-    columnWidth2: -3.333_333_333_333_333_5,
-    columnWidth3: -3.333_333_333_333_333_5,
+    columnWidth1: -3.3333333333333335,
+    columnWidth2: -3.3333333333333335,
+    columnWidth3: -13.333333333333334,
+    maxVisibleItems: 7,
+    resizerOneLeft: 6.666666666666666,
+    resizerTwoLeft: 3.3333333333333326,
+    scrollBarHeight: 0,
+    visibleItems: [],
   })
 })
 
@@ -78,7 +88,12 @@ test('resize - small width', () => {
     ...dimensions,
     columnWidth1: 20,
     columnWidth2: 20,
-    columnWidth3: 20,
+    columnWidth3: -10,
+    maxVisibleItems: 12,
+    resizerOneLeft: 50,
+    resizerTwoLeft: 70,
+    scrollBarHeight: 0,
+    visibleItems: [],
   })
 })
 
@@ -102,8 +117,13 @@ test('resize - large width', () => {
   expect(newState).toEqual({
     ...state,
     ...dimensions,
-    columnWidth1: 383.333_333_333_333_3,
-    columnWidth2: 383.333_333_333_333_3,
-    columnWidth3: 383.333_333_333_333_3,
+    columnWidth1: 383.3333333333333,
+    columnWidth2: 383.3333333333333,
+    columnWidth3: 333.3333333333333,
+    maxVisibleItems: 37,
+    resizerOneLeft: 433.3333333333333,
+    resizerTwoLeft: 816.6666666666666,
+    scrollBarHeight: 0,
+    visibleItems: [],
   })
 })
