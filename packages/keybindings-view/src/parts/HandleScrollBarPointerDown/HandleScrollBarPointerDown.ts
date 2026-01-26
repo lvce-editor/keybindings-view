e08c7d1a44292b58954cdc002de93c2bdb5e0626
Le Vivilet <levivilet@pm.me>
2026-01-26 11:45:46 +0100

    feature: on scrollbar pointer down, move to that position

--- packages/keybindings-view/src/parts/HandleScrollBarPointerDown/HandleScrollBarPointerDown.ts
+++ packages/keybindings-view/src/parts/HandleScrollBarPointerDown/HandleScrollBarPointerDown.ts
@@ -1,8 +1,26 @@
 import type { KeyBindingsState } from '../KeyBindingsState/KeyBindingsState.ts'
+import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
+import { getNewDeltaPercent } from '../ScrollBarFunctions/ScrollBarFunctions.ts'
+import { setDeltaY } from '../SetDeltaY/SetDeltaY.ts'
 
-export const handleScrollBarPointerDown = (state: KeyBindingsState, clientX: number, clientY: number): KeyBindingsState => {
+export const handleScrollBarPointerDown = (state: KeyBindingsState, eventX: number, eventY: number): KeyBindingsState => {
+  const { deltaY, finalDeltaY, headerHeight, height, scrollBarHeight, y } = state
+  const contentHeight = height - headerHeight
+  const relativeY = eventY - y - headerHeight
+  const currentScrollBarY = ScrollBarFunctions.getScrollBarY(deltaY, finalDeltaY, contentHeight, scrollBarHeight)
+  const diff = relativeY - currentScrollBarY
+  if (diff >= 0 && diff < scrollBarHeight) {
+    return {
+      ...state,
+      handleOffset: diff,
+      scrollBarActive: true,
+    }
+  }
+  const { handleOffset, percent } = getNewDeltaPercent(contentHeight, scrollBarHeight, relativeY)
+  const newDeltaY = percent * finalDeltaY
   return {
-    ...state,
-    scrollBarPointerDown: true,
+    ...setDeltaY(state, newDeltaY),
+    handleOffset,
+    scrollBarActive: true,
   }
 }
