b979cc255c314176061011e155ffecae3b05ac5b
Le Vivilet <levivilet@pm.me>
2026-08-01 13:08:42 -0700

    feature: update eslint dependencies (#687)

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedVirtualDom,
   {
@@ -31,4 +32,4 @@ export default [
       'virtual-dom/prefer-state-destructuring': 'off',
     },
   },
-]
+])
